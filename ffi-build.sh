daab60b9b723503d37330c8d364e1f8b66a92b38
Levi Morrison <levi.morrison@datadoghq.com>
2022-01-03 14:45:07 -0700

    Adjust per-platform link libraries (#12)
    * Adjust per-platform link libraries
    
    While investigating a sigsegv due to strange linking, I realized that
    the link libraries that Rust suggests are per platform. This commit
    adjusts the pkg-config and cmake helpers in a per-platform way.
    
    Note that this is brittle. Any change in these flags should be
    investigated by a human.
    
    * Remove -lgcc_s for Linux platforms
    
    This is because if it is present, even if -static-libgcc is provided,
    then it will be present in the final library runtime dependencies.
    This is undesirable at least on Alpine Linux, where it may not even be
    present due to the platform's minimalism.

--- ffi-build.sh
+++ ffi-build.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 
-# Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
-# This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021-Present Datadog, Inc.
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the Apache License Version 2.0. This product includes software developed
+# at Datadog (https://www.datadoghq.com/). Copyright 2021-Present Datadog, Inc.
 
 set -eu
 
@@ -10,15 +11,66 @@ destdir="$1"
 mkdir -v -p "$destdir/include/ddprof" "$destdir/lib/pkgconfig" "$destdir/cmake"
 
 version=$(awk -F\" '$1 ~ /^version/ { print $2 }' < ddprof-ffi/Cargo.toml)
-sed "s/@version@/${version}/g" < ddprof_ffi.pc.in > "$destdir/lib/pkgconfig/ddprof_ffi.pc"
-cp -v cmake/DDProfConfig.cmake "$destdir/cmake/"
+target="$(rustc -vV | awk '/^host:/ { print $2 }')"
+
+# Rust provides this note about the link libraries:
+# note: Link against the following native artifacts when linking against this
+# static library. The order and any duplication can be significant on some
+# platforms.
+#
+# We've decided to strip out -lgcc_s because if it's provided then it will
+# always make it into the final runtime dependencies, even if -static-libgcc is
+# provided. At least on Alpine, libgcc_s may not even exist in the users'
+# images, so -static-libgcc is recommended there.
+case "$target" in
+    "x86_64-alpine-linux-musl")
+        expected_native_static_libs=" -lssp_nonshared -lgcc_s -lc"
+        native_static_libs=" -lssp_nonshared -lc"
+        ;;
+    "x86_64-apple-darwin")
+        expected_native_static_libs=" -framework Security -liconv -lSystem -lresolv -lc -lm -liconv"
+        native_static_libs="${expected_native_static_libs}"
+        ;;
+    "x86_64-unknown-linux-gnu")
+        expected_native_static_libs=" -ldl -lrt -lpthread -lgcc_s -lc -lm -lrt -lpthread -lutil -ldl -lutil"
+        native_static_libs=" -ldl -lrt -lpthread -lc -lm -lrt -lpthread -lutil -ldl -lutil"
+        ;;
+    *)
+        >&2 echo "Unknown platform '${target}'"
+        exit 1
+        ;;
+esac
+
+echo "Recognized platform '${target}'. Adding libs: ${native_static_libs}"
+sed < ddprof_ffi.pc.in "s/@DDProf_FFI_VERSION@/${version}/g" \
+    | sed "s/@DDProf_FFI_LIBRARIES@/${native_static_libs}/g" \
+    > "$destdir/lib/pkgconfig/ddprof_ffi.pc"
+
+sed < cmake/DDProfConfig.cmake.in \
+    > $destdir/cmake/DDProfConfig.cmake \
+    "s/@DDProf_FFI_LIBRARIES@/${native_static_libs}/g"
+
 cp -v LICENSE LICENSE-3rdparty.yml NOTICE "$destdir/"
 
-RUSTFLAGS="${RUSTFLAGS:- -C relocation-model=pic}" cargo build --release
-cp -v target/release/libddprof_ffi.a "$destdir/lib/"
+export RUSTFLAGS="${RUSTFLAGS:- -C relocation-model=pic}"
 
+echo "Building the libddprof_ffi.a library (may take some time)..."
+cargo build --release --target ${target}
+cp -v target/${target}/release/libddprof_ffi.a "$destdir/lib/"
+
+echo "Checking that native-static-libs are as expected for this platform..."
+cd ddprof-ffi
+actual_native_static_libs="$(cargo rustc --release --target ${target} -- --print=native-static-libs 2>&1 | awk -F ':' '/note: native-static-libs:/ { print $3 }')"
+echo "Actual native-static-libs:${actual_native_static_libs}"
+echo "Expected native-static-libs:${expected_native_static_libs}"
+
+[ "${expected_native_static_libs}" = "${actual_native_static_libs}" ]
+cd -
+
+echo "Generating the ddprof/ffi.h header..."
 cbindgen --crate ddprof-ffi --config ddprof-ffi/cbindgen.toml --output "$destdir/include/ddprof/ffi.h"
 
 # CI doesn't have any clang tooling
 # clang-format -i "$destdir/include/ddprof/ffi.h"
 
+echo "Done."
