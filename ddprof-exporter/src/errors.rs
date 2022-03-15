6dd1f8b37a337e18e5807487fa59c2a494bf95cd
paullegranddc <82819397+paullegranddc@users.noreply.github.com>
2022-03-15 16:17:39 +0100

    Switch ddprof http client (#32)
    * Switch the http client from reqwest to hyper
    * Create a Connector that can handle unix socket streams
    * Add rustls hyper support
    * Rename Client to HttpClient to work around cbindgen
    * Better error message when the request times out
    * Add tests for uds handling
    * skip uds feature on non unix systems because tokio doesn't support it
    * Switch the multipart implementation to hyper-multipart-rfc7578
    * Enable tls1.2 on rustls
    
    Co-authored-by: Levi Morrison <levi.morrison@datadoghq.com>

---
+++ ddprof-exporter/src/errors.rs
@@ -0,0 +1,22 @@
+use std::error;
+use std::fmt;
+
+#[derive(Clone, Debug)]
+#[allow(dead_code)]
+pub(crate) enum Error {
+    InvalidUrl,
+    OperationTimedOut,
+    UnixSockeUnsuported
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(match self {
+            Self::InvalidUrl => "invalid url",
+            Self::OperationTimedOut => "operation timed out",
+            Self::UnixSockeUnsuported => "unix sockets unsuported on windows"
+        })
+    }
+}
+
+impl error::Error for Error {}
