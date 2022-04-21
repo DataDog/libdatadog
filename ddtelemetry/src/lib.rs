d1840c863cc3d0d3a4f77db1180d2d810a010d79
paullegranddc <82819397+paullegranddc@users.noreply.github.com>
2022-04-21 14:14:18 +0200

    Fix failing gitlab CI lints (#57)
    * Update 3rd party license file
    
    * Allow mutex_atomic clippy lint
    motivations: https://github.com/rust-lang/rust-clippy/issues/4295

--- ddtelemetry/src/lib.rs
+++ ddtelemetry/src/lib.rs
@@ -1,5 +1,6 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021-Present Datadog, Inc.
+#![allow(clippy::mutex_atomic)]
 
 use std::{
     sync::atomic::{AtomicU64, Ordering},
