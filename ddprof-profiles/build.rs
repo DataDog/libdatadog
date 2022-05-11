4c50575b0dafe922a1e072b9c9a1235887192929
Levi Morrison <levi.morrison@datadoghq.com>
2022-05-11 10:50:59 -0600

    Use pprof::* types directly in more places
    This reduces the number of object conversions, so we end up with less
    lines of code.
    
    This also bumps a variety of versions, editions, and dependencies. This
    probably should have been a separate PR, sorry.
    
    Update license with cargo-bundle-license 0.5.0

--- ddprof-profiles/build.rs
+++ ddprof-profiles/build.rs
@@ -1,9 +1,22 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021-Present Datadog, Inc.
 
+use prost_build::Config;
+
 fn main() -> Result<(), std::io::Error> {
+    let mut config = Config::new();
+    config
+        .type_attribute("pprof.Function", "#[derive(Copy, Eq, Hash)]")
+        .type_attribute("pprof.Label", "#[derive(Copy, Eq, Hash)]")
+        .type_attribute("pprof.Line", "#[derive(Copy, Eq, Hash)]")
+        .type_attribute("pprof.Location", "#[derive(Eq, Hash)]")
+        .type_attribute("pprof.Mapping", "#[derive(Eq, Hash)]")
+        .type_attribute("pprof.Profile", "#[derive(Eq, Hash)]")
+        .type_attribute("pprof.Sample", "#[derive(Eq, Hash)]")
+        .type_attribute("pprof.ValueType", "#[derive(Copy, Eq, Hash)]");
+
     let protos = &[concat!(env!("CARGO_MANIFEST_DIR"), "/src/profile.proto")];
     let includes = &[concat!(env!("CARGO_MANIFEST_DIR"), "/src")];
-    prost_build::compile_protos(protos, includes)?;
+    config.compile_protos(protos, includes)?;
     Ok(())
 }
