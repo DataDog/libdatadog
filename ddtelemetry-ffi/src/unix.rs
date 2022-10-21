ec699a65b9f2c0fb0be0e2bcb26ac36403081f91
Bob Weinand <bob.weinand@datadoghq.com>
2022-10-21 21:43:31 +0200

    Fix cbindgen: ensure PlatformHandles are opaque

--- ddtelemetry-ffi/src/unix.rs
+++ ddtelemetry-ffi/src/unix.rs
@@ -1,45 +1,50 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021-Present Datadog, Inc.
 
-use std::{
-    fs::File,
-    os::unix::{net::UnixStream, prelude::FromRawFd},
-};
+use std::{fs::File, mem, os::unix::{net::UnixStream, prelude::FromRawFd}};
 
 use ddtelemetry::ipc::{platform::PlatformHandle, sidecar};
 
 use crate::{try_c, MaybeError};
 
+pub struct NativeFile {
+    handle: Box<PlatformHandle<File>>
+}
+
+pub struct NativeUnixStream {
+    handle: PlatformHandle<UnixStream>
+}
+
 #[no_mangle]
 #[allow(clippy::missing_safety_doc)]
-pub unsafe extern "C" fn ddog_ph_file_from(file: *mut libc::FILE) -> Box<PlatformHandle<File>> {
+pub unsafe extern "C" fn ddog_ph_file_from(file: *mut libc::FILE) -> NativeFile {
     let handle = PlatformHandle::from_raw_fd(libc::fileno(file));
 
-    Box::from(handle)
+    NativeFile { handle: Box::from( handle) }
 }
 
 #[no_mangle]
 pub extern "C" fn ddog_ph_file_clone(
-    platform_handle: &PlatformHandle<File>,
-) -> Box<PlatformHandle<File>> {
-    Box::new(platform_handle.clone())
+    platform_handle: &NativeFile,
+) -> Box<NativeFile> {
+    Box::new(NativeFile { handle: platform_handle.handle.clone() })
 }
 
 #[no_mangle]
-pub extern "C" fn ddog_ph_file_drop(ph: Box<PlatformHandle<File>>) {
+pub extern "C" fn ddog_ph_file_drop(ph: NativeFile) {
     drop(ph)
 }
 
 #[no_mangle]
-pub extern "C" fn ddog_ph_unix_stream_drop(ph: Box<PlatformHandle<UnixStream>>) {
+pub extern "C" fn ddog_ph_unix_stream_drop(ph: Box<NativeUnixStream>) {
     drop(ph)
 }
 
 #[no_mangle]
 pub extern "C" fn ddog_sidecar_connect(
-    connection: &mut *mut PlatformHandle<UnixStream>,
+    connection: &mut *mut NativeUnixStream,
 ) -> MaybeError {
-    let stream = Box::new(try_c!(sidecar::start_or_connect_to_sidecar()).into());
+    let stream = Box::new(NativeUnixStream { handle: try_c!(sidecar::start_or_connect_to_sidecar()).into() });
     *connection = Box::into_raw(stream);
 
     MaybeError::None
@@ -61,9 +66,9 @@ mod test_c_sidecar {
 
         let file = unsafe { libc::fopen(fname.as_ptr(), mode.as_ptr()) };
         let file = unsafe { ddog_ph_file_from(file) };
-        let fd = file.as_raw_fd();
+        let fd = file.handle.as_raw_fd();
         {
-            let mut file = &*file.as_filelike_view().unwrap();
+            let mut file = &*file.handle.as_filelike_view().unwrap();
             writeln!(file, "test").unwrap();
         }
         ddog_ph_file_drop(file);
@@ -78,7 +83,7 @@ mod test_c_sidecar {
         assert_eq!(ddog_sidecar_connect(&mut connection), MaybeError::None);
         let connection = unsafe { Box::from_raw(connection) };
         {
-            let mut c = &*connection.as_socketlike_view().unwrap();
+            let mut c = &*connection.handle.as_socketlike_view().unwrap();
             writeln!(c, "test").unwrap();
             let mut buf = [0; 4];
             c.read_exact(&mut buf).unwrap();
