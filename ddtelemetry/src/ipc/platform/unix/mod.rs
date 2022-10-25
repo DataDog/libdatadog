7bc812a6f3a1dc03b1480bcb2b74a8a6b472af94
Pawel Chojnacki <pawelchcki@gmail.com>
2022-10-25 14:20:56 +0200

    Foundation for the future IPC framework
    Includes tools for safe handling of file descriptors, locks and forking
    a daemon subprocess / sidecar.
    
    With the recent stabilization of https://doc.rust-lang.org/std/os/unix/io/struct.OwnedFd.html I've removed some no longer necessary abstractions that basically reimplemented OwnedFd. I've used io-lifetimes crate to backport the implementation to older rust versions.
    
    The two biggest parts of this PR that lay groundwork for the IPC mechanism are:
    
    PlatformHandle<T> - which allows safe, ref-counted sharing of a FD both within the process and also facilitates transferring it between processes.
    
    it can be used across threads to send data through a shared socket without using locks
    avoiding mutexes is important as the process could be forked (e.g. in PHP) at anytime
    Liaison trait - which allows IPC to be set up between processes
    
    it implements mechanism to establish a shared named socket on the system
    only one process will take ownership of the listener type on the socket
    the rest will only connect to it when needed

---
+++ ddtelemetry/src/ipc/platform/unix/mod.rs
@@ -0,0 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021-Present Datadog, Inc.
+
+mod platform_handle;
+pub use platform_handle::*;
+
+pub mod locks;
+pub mod sockets;
