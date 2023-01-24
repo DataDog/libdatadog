7ecb515bb10ddbe56169e6aecde3abcd8ba0ef23
Levi Morrison <levi.morrison@datadoghq.com>
2023-01-24 16:42:15 -0700

    Remove u63

--- profiling/src/profile2/mod.rs
+++ profiling/src/profile2/mod.rs
@@ -6,11 +6,9 @@ mod prof_table;
 mod profile;
 mod string_table;
 mod symbol_table;
-mod u63;
 
 pub use pprof::{Function, Label, Line, Location, Mapping, ValueType};
 pub use prof_table::*;
 pub use profile::*;
 pub use string_table::*;
 pub use symbol_table::*;
-pub use u63::*;
