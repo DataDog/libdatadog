8be3edb520d7906ae16dc268b6a36c3d5219ddd3
David Lee <david.lee@datadoghq.com>
2023-01-25 09:38:17 -0800

    relocate normalization files

--- serverless/src/lib.rs
+++ serverless/src/lib.rs
@@ -18,20 +18,20 @@ pub mod pb {
     include!("pb.rs");
 }
 
-mod normalize_tests {
-    include!("./tests/normalize_tests.rs");
+mod normalize_utils_tests {
+    include!("./normalization/tests/normalize_utils_tests.rs");
 }
 
 pub mod normalizer {
-    include!("./normalizer.rs");
+    include!("./normalization/normalizer.rs");
 }
 
 pub mod normalize_utils {
-    include!("./normalize_utils.rs");
+    include!("./normalization/normalize_utils.rs");
 }
 
 pub mod errors {
-    include!("./errors.rs");
+    include!("./normalization/errors.rs");
 }
 
 #[macro_use]
