7c40f29383835a22c1a674587a1ee953e5af612a
David Lee <david.lee@datadoghq.com>
2023-02-01 12:41:04 -0800

    reorganize directory structure (move tests into the files they test)

--- trace-normalization/src/lib.rs
+++ trace-normalization/src/lib.rs
@@ -6,21 +6,9 @@
 #![deny(clippy::all)]
 
 pub mod pb {
-    include!("./pb.rs");
+    include!("./pb/pb.rs");
 }
 
-mod normalizer_tests {
-    include!("./normalization/tests/normalizer_tests.rs");
-}
+pub mod normalizer;
 
-mod normalize_utils_tests {
-    include!("./normalization/tests/normalize_utils_tests.rs");
-}
-
-pub mod normalizer {
-    include!("./normalization/normalizer.rs");
-}
-
-pub mod normalize_utils {
-    include!("./normalization/normalize_utils.rs");
-}
+pub mod normalize_utils;
--- trace-normalization/src/normalize_utils.rs
+++ trace-normalization/src/normalize_utils.rs
@@ -173,3 +173,56 @@ pub fn is_alpha(c: char) -> bool {
 pub fn is_alpha_num(c: char) -> bool {
     is_alpha(c) || ('0'..='9').contains(&c)
 }
+
+#[cfg(test)]
+mod tests {
+
+    use crate::normalize_utils;
+
+    #[test]
+    fn test_normalize_name() {
+        let test_tuples: [(&str, &str, &str); 5] = [
+            (
+                "",
+                "",
+                "Normalizer Error: Empty"
+            ),
+            (
+                "good",
+                "good",
+                "",
+            ),
+            (
+                "Too-Long-.Too-Long-.Too-Long-.Too-Long-.Too-Long-.Too-Long-.Too-Long-.Too-Long-.Too-Long-.Too-Long-.Too-Long-.",
+                "Too_Long.Too_Long.Too_Long.Too_Long.Too_Long.Too_Long.Too_Long.Too_Long.Too_Long.Too_Long.",
+                "",
+            ),
+            (
+                "bad-name",
+                "bad_name",
+                "",
+            ),
+            (
+                "&",
+                "",
+                "Normalizer Error: Invalid"
+            )
+        ];
+
+        for tuple in test_tuples.iter() {
+            let input = tuple.0;
+            let expected = tuple.1;
+            let expected_err = tuple.2;
+
+            match normalize_utils::normalize_name(input.to_string()) {
+                Ok(val) => {
+                    assert_eq!(expected_err, "");
+                    assert_eq!(val, expected);
+                },
+                Err(err) => {
+                    assert_eq!(format!("{}", err), expected_err);
+                }
+            }
+        }
+    }
+}
