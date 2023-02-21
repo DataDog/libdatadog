946b72b13310194f5843494bc72898b1ebe16e11
Pawel Chojnacki <pawelchcki@gmail.com>
2023-02-21 13:50:29 +0100

    Update license files

--- ddtelemetry/build.rs
+++ ddtelemetry/build.rs
@@ -1,3 +1,6 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021-Present Datadog, Inc.
+
 fn main() {
     // ensure symbols are properly exported for dlsym to be able to look them up
     println!("cargo:rustc-link-arg-tests=-rdynamic")
