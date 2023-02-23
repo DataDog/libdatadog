baf1c55bd36b58e6dcee8c7736030c11a89f96b2
David Lee <david.lee@datadoghq.com>
2023-02-22 16:47:12 -0800

    fix CI

--- trace-obfuscation/benches/replace_trace_tags_benchmark.rs
+++ trace-obfuscation/benches/replace_trace_tags_benchmark.rs
@@ -1,3 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0. This product includes software
+// developed at Datadog (https://www.datadoghq.com/). Copyright 2023-Present
+// Datadog, Inc.
+
 use std::collections::HashMap;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
