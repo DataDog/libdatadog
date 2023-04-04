664e1ff729876f01ec93d75aeff94fcedad32f86
David Lee <thedavl2001@gmail.com>
2023-04-04 16:08:41 -0700

    Serverless Trace Mini Agent (#124)

---
+++ trace-mini-agent/src/mini_agent.rs
@@ -0,0 +1,87 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2023-Present Datadog, Inc.
+
+use datadog_trace_protobuf::pb;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use std::sync::Arc;
+
+use tokio::sync::mpsc::{self, Receiver, Sender};
+
+use crate::{trace_flusher, trace_processor};
+
+const MINI_AGENT_PORT: usize = 8126;
+const TRACE_ENDPOINT_PATH: &str = "/v0.4/traces";
+const TRACER_PAYLOAD_CHANNEL_BUFFER_SIZE: usize = 10;
+
+pub struct MiniAgent {
+    pub trace_processor: Arc<dyn trace_processor::TraceProcessor + Send + Sync>,
+    pub trace_flusher: Arc<dyn trace_flusher::TraceFlusher + Send + Sync>,
+}
+
+impl MiniAgent {
+    #[tokio::main]
+    pub async fn start_mini_agent(&self) -> Result<(), Box<dyn std::error::Error>> {
+        // setup a channel to send processed traces to our flusher
+        // tx is passed through each endpoint_handler to the trace processor, which uses it to send de-serialized processed
+        // trace payloads to our trace flusher.
+        let (tx, rx): (Sender<pb::TracerPayload>, Receiver<pb::TracerPayload>) =
+            mpsc::channel(TRACER_PAYLOAD_CHANNEL_BUFFER_SIZE);
+
+        // start our trace flusher. receives trace payloads and handles buffering + deciding when to flush to backend.
+        let trace_flusher = self.trace_flusher.clone();
+        tokio::spawn(async move {
+            let trace_flusher = trace_flusher.clone();
+            trace_flusher.start_trace_flusher(rx).await;
+        });
+
+        // setup our hyper http server, where the endpoint_handler handles incoming requests
+        let trace_processor = self.trace_processor.clone();
+        let make_svc = make_service_fn(move |_| {
+            let trace_processor = trace_processor.clone();
+            let tx = tx.clone();
+
+            let service = service_fn(move |req| {
+                MiniAgent::trace_endpoint_handler(req, trace_processor.clone(), tx.clone())
+            });
+
+            async move { Ok::<_, Infallible>(service) }
+        });
+
+        let addr = SocketAddr::from(([127, 0, 0, 1], MINI_AGENT_PORT as u16));
+        let server = Server::bind(&addr).serve(make_svc);
+
+        // start hyper http server
+        if let Err(e) = server.await {
+            println!("Server error: {}", e);
+        }
+
+        Ok(())
+    }
+
+    async fn trace_endpoint_handler(
+        req: Request<Body>,
+        trace_processor: Arc<dyn trace_processor::TraceProcessor + Send + Sync>,
+        tx: Sender<pb::TracerPayload>,
+    ) -> Result<Response<Body>, Infallible> {
+        match (req.method(), req.uri().path()) {
+            (&Method::PUT, TRACE_ENDPOINT_PATH) => {
+                match trace_processor.process_traces(req, tx).await {
+                    Ok(res) => Ok(res),
+                    Err(err) => Ok(Response::new(Body::from(format!(
+                        "Error processing traces: {}",
+                        err
+                    )))),
+                }
+            }
+            _ => {
+                println!("{}", req.uri().path());
+                let mut not_found = Response::default();
+                *not_found.status_mut() = StatusCode::NOT_FOUND;
+                Ok(not_found)
+            }
+        }
+    }
+}
---
+++ trace-utils/src/trace_utils.rs
@@ -0,0 +1,275 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2023-Present Datadog, Inc.
+
+use hyper::http::HeaderValue;
+use hyper::HeaderMap;
+use hyper::{body::Buf, Body, Client, Method, Request};
+use hyper_rustls::HttpsConnectorBuilder;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::{env, str};
+
+use prost::Message;
+
+use datadog_trace_protobuf::pb;
+
+const TRACE_INTAKE_URL: &str = "https://trace.agent.datadoghq.com/api/v0.2/traces";
+
+macro_rules! parse_header {
+    (
+        $header_map:ident,
+        { $($header:literal => $($field:ident).+ ,)+ }
+    ) => {
+        $(
+            if let Some(h) = $header_map.get($header) {
+                if let Ok(h) = h.to_str() {
+                    $($field).+ = h;
+                }
+            }
+        )+
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
+pub struct Span {
+    service: Option<String>,
+    name: String,
+    resource: String,
+    trace_id: u64,
+    span_id: u64,
+    parent_id: Option<u64>,
+    start: i64,
+    duration: i64,
+    error: Option<i32>,
+    meta: HashMap<String, String>,
+    metrics: Option<HashMap<String, f64>>,
+}
+
+pub async fn get_traces_from_request_body(body: Body) -> anyhow::Result<Vec<Vec<pb::Span>>> {
+    let buffer = hyper::body::aggregate(body).await.unwrap();
+
+    let traces: Vec<Vec<Span>> = match rmp_serde::from_read(buffer.reader()) {
+        Ok(res) => res,
+        Err(err) => {
+            anyhow::bail!("Error deserializing trace from request body: {}", err)
+        }
+    };
+
+    let mut pb_traces = Vec::<Vec<pb::Span>>::new();
+    for trace in traces {
+        let mut pb_spans = Vec::<pb::Span>::new();
+        for span in trace {
+            let span = pb::Span {
+                service: span.service.unwrap_or_default(),
+                name: span.name,
+                resource: span.resource,
+                trace_id: span.trace_id,
+                span_id: span.span_id,
+                parent_id: span.parent_id.unwrap_or_default(),
+                start: span.start,
+                duration: span.duration,
+                error: span.error.unwrap_or(0),
+                meta: span.meta,
+                meta_struct: HashMap::new(),
+                metrics: span.metrics.unwrap_or_default(),
+                r#type: "custom".to_string(),
+            };
+
+            pb_spans.push(span);
+        }
+        if !pb_spans.is_empty() {
+            pb_traces.push(pb_spans);
+        }
+    }
+
+    if pb_traces.is_empty() {
+        anyhow::bail!("No traces deserialized from the request body.")
+    }
+
+    Ok(pb_traces)
+}
+
+#[derive(Default)]
+pub struct TracerTags<'a> {
+    lang: &'a str,
+    lang_version: &'a str,
+    lang_interpreter: &'a str,
+    lang_vendor: &'a str,
+    tracer_version: &'a str,
+}
+
+pub fn get_tracer_tags_from_request_header(headers: &HeaderMap<HeaderValue>) -> TracerTags {
+    let mut tags = TracerTags::default();
+    parse_header!(
+        headers,
+        {
+            "datadog-meta-lang" => tags.lang,
+            "datadog-meta-lang-version" => tags.lang_version,
+            "datadog-meta-lang-interpreter" => tags.lang_interpreter,
+            "datadog-meta-lang-vendor" => tags.lang_vendor,
+            "datadog-meta-tracer-version" => tags.tracer_version,
+        }
+    );
+    tags
+}
+
+pub fn construct_agent_payload(tracer_payloads: Vec<pb::TracerPayload>) -> pb::AgentPayload {
+    pb::AgentPayload {
+        host_name: "ffi-test-hostname".to_string(),
+        env: "ffi-test-env".to_string(),
+        agent_version: "ffi-agent-version".to_string(),
+        error_tps: 60.0,
+        target_tps: 60.0,
+        tags: HashMap::new(),
+        tracer_payloads,
+    }
+}
+
+pub fn construct_trace_chunk(trace: Vec<pb::Span>) -> pb::TraceChunk {
+    pb::TraceChunk {
+        priority: 1,
+        origin: "".to_string(),
+        spans: trace,
+        tags: HashMap::new(),
+        dropped_trace: false,
+    }
+}
+
+pub fn construct_tracer_payload(
+    chunks: Vec<pb::TraceChunk>,
+    tracer_tags: TracerTags,
+) -> pb::TracerPayload {
+    pb::TracerPayload {
+        app_version: "placeholder_version".to_string(),
+        language_name: tracer_tags.lang.to_string(),
+        container_id: "".to_string(),
+        env: "placeholder_env".to_string(),
+        runtime_id: "".to_string(),
+        chunks,
+        hostname: "".to_string(),
+        language_version: tracer_tags.lang_version.to_string(),
+        tags: HashMap::new(),
+        tracer_version: tracer_tags.tracer_version.to_string(),
+    }
+}
+
+pub fn serialize_agent_payload(payload: pb::AgentPayload) -> Vec<u8> {
+    let mut buf = Vec::new();
+    buf.reserve(payload.encoded_len());
+    payload.encode(&mut buf).unwrap();
+    buf
+}
+
+pub async fn send(data: Vec<u8>) -> anyhow::Result<()> {
+    let api_key = match env::var("DD_API_KEY") {
+        Ok(key) => key,
+        Err(_) => anyhow::bail!("oopsy, no DD_API_KEY was provided"),
+    };
+
+    let req = Request::builder()
+        .method(Method::POST)
+        .uri(TRACE_INTAKE_URL)
+        .header("User-agent", "ffi-test")
+        .header("Content-type", "application/x-protobuf")
+        .header("DD-API-KEY", &api_key)
+        .header("X-Datadog-Reported-Languages", "nodejs")
+        .body(Body::from(data))?;
+
+    let https = HttpsConnectorBuilder::new()
+        .with_native_roots()
+        .https_only()
+        .enable_http1()
+        .build();
+    let client: Client<_, hyper::Body> = Client::builder().build(https);
+    match client.request(req).await {
+        Ok(_) => {
+            println!("Successfully sent traces");
+        }
+        Err(e) => println!("Failed to send traces: {}", e),
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use datadog_trace_protobuf::pb;
+    use serde_json::json;
+
+    use hyper::Request;
+
+    use crate::trace_utils;
+
+    #[tokio::test]
+    async fn test_get_traces_from_request_body() {
+        let pairs = vec![
+            (
+                json!([{
+                    "service": "test-service",
+                    "name": "test-service-name",
+                    "resource": "test-service-resource",
+                    "trace_id": 111,
+                    "span_id": 222,
+                    "parent_id": 333,
+                    "start": 1,
+                    "duration": 5,
+                    "error": 0,
+                    "meta": {},
+                    "metrics": {},
+                }]),
+                vec![vec![pb::Span {
+                    service: "test-service".to_string(),
+                    name: "test-service-name".to_string(),
+                    resource: "test-service-resource".to_string(),
+                    trace_id: 111,
+                    span_id: 222,
+                    parent_id: 333,
+                    start: 1,
+                    duration: 5,
+                    error: 0,
+                    meta: HashMap::new(),
+                    metrics: HashMap::new(),
+                    meta_struct: HashMap::new(),
+                    r#type: "custom".to_string(),
+                }]],
+            ),
+            (
+                json!([{
+                    "name": "test-service-name",
+                    "resource": "test-service-resource",
+                    "trace_id": 111,
+                    "span_id": 222,
+                    "start": 1,
+                    "duration": 5,
+                    "meta": {},
+                }]),
+                vec![vec![pb::Span {
+                    service: "".to_string(),
+                    name: "test-service-name".to_string(),
+                    resource: "test-service-resource".to_string(),
+                    trace_id: 111,
+                    span_id: 222,
+                    parent_id: 0,
+                    start: 1,
+                    duration: 5,
+                    error: 0,
+                    meta: HashMap::new(),
+                    metrics: HashMap::new(),
+                    meta_struct: HashMap::new(),
+                    r#type: "custom".to_string(),
+                }]],
+            ),
+        ];
+
+        for (trace_input, output) in pairs {
+            let bytes = rmp_serde::to_vec(&vec![&trace_input]).unwrap();
+            let request = Request::builder()
+                .body(hyper::body::Body::from(bytes))
+                .unwrap();
+            let res = trace_utils::get_traces_from_request_body(request.into_body()).await;
+            assert!(res.is_ok());
+            assert_eq!(res.unwrap(), output);
+        }
+    }
+}
