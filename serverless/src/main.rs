8ceb1e888b2477d583597dd971bbc58643de87b6
David Lee <david.lee@datadoghq.com>
2023-04-11 12:49:03 -0700

    create stats_processor

--- serverless/src/main.rs
+++ serverless/src/main.rs
@@ -5,7 +5,7 @@ use env_logger::{Builder, Env, Target};
 use log::{error, info};
 use std::sync::Arc;
 
-use datadog_trace_mini_agent::{mini_agent, trace_flusher, trace_processor};
+use datadog_trace_mini_agent::{mini_agent, stats_processor, trace_flusher, trace_processor};
 
 pub fn main() {
     let env = Env::new().filter_or("DD_LOG_LEVEL", "info");
@@ -18,9 +18,12 @@ pub fn main() {
 
     let trace_processor = Arc::new(trace_processor::ServerlessTraceProcessor {});
 
+    let stats_processor = Arc::new(stats_processor::ServerlessStatsProcessor {});
+
     let mini_agent = Box::new(mini_agent::MiniAgent {
         trace_processor,
         trace_flusher,
+        stats_processor,
     });
 
     if let Err(e) = mini_agent.start_mini_agent() {
