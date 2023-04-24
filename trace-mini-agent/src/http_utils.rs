80f6cf7cba87308f0b8f2b77cabff22119ff3a3c
David Lee <david.lee@datadoghq.com>
2023-04-24 13:44:34 -0700

    add trace metrics endpoint

---
+++ trace-mini-agent/src/http_utils.rs
@@ -0,0 +1,32 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2023-Present Datadog, Inc.
+
+use hyper::{http, Body, Response, StatusCode};
+use log::{error, info};
+use serde_json::json;
+
+/// Logs a message (at the info level) and returns the same message in the body of response with status code 200.
+/// Response body:
+/// {
+///     "message": message
+/// }
+pub fn log_and_return_http_success_response(message: &str) -> http::Result<Response<Body>> {
+    info!("{message}");
+    let body = json!({ "message": message }).to_string();
+    Response::builder()
+        .status(StatusCode::OK)
+        .body(Body::from(body))
+}
+
+/// Logs a message (at the error level) and returns the same message in the body of response with status code 500.
+/// Response body:
+/// {
+///     "message": message
+/// }
+pub fn log_and_return_http_error_response(message: &str) -> http::Result<Response<Body>> {
+    error!("{message}");
+    let body = json!({ "message": message }).to_string();
+    Response::builder()
+        .status(StatusCode::INTERNAL_SERVER_ERROR)
+        .body(Body::from(body))
+}
---
+++ trace-mini-agent/src/stats_processor.rs
@@ -0,0 +1,73 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2023-Present Datadog, Inc.
+
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use async_trait::async_trait;
+use hyper::{http, Body, Request, Response};
+use log::info;
+use tokio::sync::mpsc::Sender;
+
+use datadog_trace_protobuf::pb;
+use datadog_trace_utils::stats_utils;
+
+use crate::http_utils::{log_and_return_http_error_response, log_and_return_http_success_response};
+
+#[async_trait]
+pub trait StatsProcessor {
+    /// Deserializes trace stats from a hyper request body and sends them through
+    /// the provided tokio mpsc Sender.
+    async fn process_stats(
+        &self,
+        req: Request<Body>,
+        tx: Sender<pb::ClientStatsPayload>,
+    ) -> http::Result<Response<Body>>;
+}
+
+#[derive(Clone)]
+pub struct ServerlessStatsProcessor {}
+
+#[async_trait]
+impl StatsProcessor for ServerlessStatsProcessor {
+    async fn process_stats(
+        &self,
+        req: Request<Body>,
+        tx: Sender<pb::ClientStatsPayload>,
+    ) -> http::Result<Response<Body>> {
+        let body = req.into_body();
+
+        info!("Recieved trace stats to process");
+
+        // deserialize trace stats from the request body, convert to protobuf structs (see trace-protobuf crate)
+        let mut stats: pb::ClientStatsPayload =
+            match stats_utils::get_stats_from_request_body(body).await {
+                Ok(res) => res,
+                Err(err) => {
+                    return log_and_return_http_error_response(&format!(
+                        "Error deserializing trace stats from request body: {err}"
+                    ));
+                }
+            };
+
+        let start = SystemTime::now();
+        let timestamp = start
+            .duration_since(UNIX_EPOCH)
+            .unwrap_or_default()
+            .as_nanos();
+        stats.stats[0].start = timestamp as u64;
+
+        // send trace payload to our trace flusher
+        match tx.send(stats).await {
+            Ok(_) => {
+                return log_and_return_http_success_response(
+                    "Successfully buffered stats to be flushed.",
+                );
+            }
+            Err(err) => {
+                return log_and_return_http_error_response(&format!(
+                    "Error sending stats to the stats flusher: {err}"
+                ));
+            }
+        }
+    }
+}
