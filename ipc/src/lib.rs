1997745de5192831d6a6039c0df6a3957b990383
Bob Weinand <bob.weinand@datadoghq.com>
2023-05-19 19:11:36 +0200

    Fixing all the outstanding issues with the sidecar (#161)
    * Make tarpc request execution sequential for a single connection
    
    * Close all fds except the ones we care about
    
    * Avoid closing the trampoline fd
    
    * Defer control of binary stubs to spawn_worker
    
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>
    
    * Silent lsan
    
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>
    
    * Avoid Future::pending() to simulate no response, but just discard it
    
    * Execute second fork only after closing all file descriptors in the forked process
    
    * Make set_session_agent_url asynchronous (do not block)
    
    * Reset mock_client_file for non-files
    
    * Fix race conditions in register_service_and_flush_queued_actions()
    
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>
    
    * Make CI happy
    
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>
    
    ---------
    
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>
    Co-authored-by: paullegranddc <paul.legranddescloizeaux@datadoghq.com>

--- ipc/src/lib.rs
+++ ipc/src/lib.rs
@@ -1,13 +1,14 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021-Present Datadog, Inc.
-#[cfg(unix)]
-pub mod handles;
-pub mod platform;
-
-#[cfg(unix)]
-pub mod transport;
 
 #[cfg(unix)]
 pub mod example_interface;
+#[cfg(unix)]
+pub mod handles;
+#[cfg(unix)]
+pub mod transport;
+
+pub mod platform;
+pub mod sequential;
 
 pub use tarpc;
---
+++ ipc/src/sequential.rs
@@ -0,0 +1,90 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021-Present Datadog, Inc.
+
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use futures::{ready, Future, Stream};
+use tarpc::{
+    self,
+    server::{Channel, InFlightRequest, Requests, Serve},
+};
+
+#[allow(type_alias_bounds)]
+type Request<S, C: Channel> = (S, InFlightRequest<C::Req, C::Resp>);
+
+/// Replaces tarpc::server::Channel::execute which spawns one task per message with an executor
+/// that spawns a single worker and queues requests for this task.
+///
+/// If the queue is full, the requests is dropped and will be cancelled by tarpc.
+pub fn execute_sequential<C, S>(
+    reqs: Requests<C>,
+    serve: S,
+    max_requests: usize,
+) -> SequentialExecutor<C, S>
+where
+    C: Channel,
+    S: Serve<C::Req, Resp = C::Resp> + Send + 'static,
+    C::Req: Send + 'static,
+    C::Resp: Send + 'static,
+    S::Fut: Send,
+{
+    let (tx, mut rx) = tokio::sync::mpsc::channel::<Request<S, C>>(max_requests);
+
+    tokio::spawn(async move {
+        loop {
+            let (serve, req) = match rx.recv().await {
+                None => return,
+                Some(s) => s,
+            };
+            req.execute(serve).await;
+        }
+    });
+    SequentialExecutor {
+        inner: reqs,
+        serve,
+        tx,
+    }
+}
+
+#[pin_project::pin_project]
+pub struct SequentialExecutor<C, S>
+where
+    C: Channel + 'static,
+{
+    #[pin]
+    inner: Requests<C>,
+    serve: S,
+    tx: tokio::sync::mpsc::Sender<Request<S, C>>,
+}
+
+impl<C, S> Future for SequentialExecutor<C, S>
+where
+    C: Channel + 'static,
+    C::Req: Send + 'static,
+    C::Resp: Send + 'static,
+    S: Serve<C::Req, Resp = C::Resp> + Send + 'static + Clone,
+    S::Fut: Send,
+{
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        while let Some(response_handler) = ready!(self.as_mut().project().inner.poll_next(cx)) {
+            match response_handler {
+                Ok(resp) => {
+                    let server = self.serve.clone();
+                    if let Err(_err) = self.as_ref().tx.try_send((server, resp)) {
+                        // TODO: should we log something in case we drop the request on the floor?
+                    }
+                }
+                Err(_e) => {
+                    // TODO: should we log something in case we drop the request on the floor?
+                    break;
+                }
+            }
+        }
+        Poll::Ready(())
+    }
+}
