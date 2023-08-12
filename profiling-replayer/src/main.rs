fd8258b0a0055e0651cf60440be051ddd2155370
Levi Morrison <levi.morrison@datadoghq.com>
2023-08-12 14:59:43 -0600

    add quartile stats

--- profiling-replayer/src/main.rs
+++ profiling-replayer/src/main.rs
@@ -13,6 +13,51 @@ use std::time::Instant;
 
 pub use replayer::*;
 
+/// Returns (_, true) if the midpoint is odd.
+fn midpoint(values: &[usize]) -> Option<(usize, bool)> {
+    if values.is_empty() {
+        return None;
+    }
+    // 4 / = 2, but in offsets, 1 is the middle ([0, 1, 2, 3])
+    let midoffset = values.len() / 2 - 1;
+    // if it's odd, get the average of the two
+    if (values.len() & 1usize) != 0 {
+        Some((midoffset, true))
+    } else {
+        Some((midoffset, false))
+    }
+}
+
+fn median(values: &[usize]) -> Option<f64> {
+    match midpoint(values) {
+        None => None,
+        Some((midpoint, is_odd)) => {
+            if is_odd {
+                Some((values[midpoint] + values[midpoint + 1]) as f64)
+            } else {
+                Some(values[midpoint] as f64)
+            }
+        }
+    }
+}
+
+/// Finds the Q1, Q2, Q3 values. Assumes the slice is sorted.
+fn quartiles(values: &[usize]) -> Option<[f64; 3]> {
+    // This calculates Q3 as the median of the values above the midpoint, which
+    // depending on how you define Q3, this is possibly not correct.
+    if values.len() < 4 {
+        return None;
+    }
+
+    let (midpoint, _) = midpoint(values).unwrap();
+    let q2 = median(values).unwrap();
+
+    let q1 = median(&values[..midpoint]).unwrap();
+    let q3 = median(&values[(midpoint + 1)..]).unwrap();
+
+    Some([q1, q2, q3])
+}
+
 fn main() -> anyhow::Result<()> {
     let matches = command!()
         .arg(
@@ -47,6 +92,31 @@ fn main() -> anyhow::Result<()> {
         .period(replayer.period.clone())
         .build();
 
+    // Before benchmarking, let's calculate some statistics.
+    // No point doing that if there aren't at least 4 samples though.
+    let n_samples = replayer.samples.len();
+    println!("Number of samples: {n_samples}.");
+    if n_samples >= 4usize {
+        let mut depths: Vec<usize> = replayer
+            .samples
+            .iter()
+            .map(|sample| sample.locations.len())
+            .collect();
+
+        depths.sort();
+        let min = depths.first().unwrap();
+        let [q1, q2, q3] = quartiles(depths.as_slice()).unwrap();
+        let median = {};
+        let max = depths.last().unwrap();
+
+        let max = depths[depths.len() - 1];
+
+        println!("Min stack depth is {min}.");
+        println!("Q1 = {q1}, Q2 = {q2}, Q3 = {q3}.");
+        println!("Max stack depth is {max}.");
+    }
+
+    // When benchmarking, don't count the copying of the stacks, do that before.
     let samples = std::mem::take(&mut replayer.samples);
     let before = Instant::now();
     for sample in samples {
