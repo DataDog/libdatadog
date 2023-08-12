cec5273f7a1e3103939046e4dfb83ece13fabfe2
Levi Morrison <levi.morrison@datadoghq.com>
2023-08-12 15:07:51 -0600

    add missing license header

--- profiling/src/profile/internal/stack_trace.rs
+++ profiling/src/profile/internal/stack_trace.rs
@@ -1,3 +1,6 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2023-Present Datadog, Inc.
+
 use super::{Id, Item, LocationId};
 use std::fmt::{Display, Formatter};
 
