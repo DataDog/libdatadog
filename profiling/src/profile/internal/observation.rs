055d1d2b1f9d8682833eee85f6f6e6ca8d3636ff
Daniel Schwartz-Narbonne <daniel.schwartznarbonne@datadoghq.com>
2023-08-15 12:44:07 -0400

    Cleanup and comment

--- profiling/src/profile/internal/observation.rs
+++ profiling/src/profile/internal/observation.rs
@@ -1,6 +1,9 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021-Present Datadog, Inc.
 
+/// This represents the set of values observed for a sample
+/// Since the length is fixed for a given Profile, no need
+/// to store the `capacity` field. Saves 8 bytes vs `Vec<i64>`.
 #[repr(transparent)]
 pub struct Observation {
     data: Box<[i64]>,
@@ -13,12 +16,14 @@ impl From<Vec<i64>> for Observation {
     }
 }
 
+impl AsRef<[i64]> for Observation {
+    fn as_ref(&self) -> &[i64] {
+        &self.data
+    }
+}
+
 impl Observation {
     pub fn iter_mut(&mut self) -> core::slice::IterMut<'_, i64> {
         self.data.iter_mut()
-    }
-
-    pub fn as_ref(&self) -> &[i64] {
-        &self.data
     }
 }
