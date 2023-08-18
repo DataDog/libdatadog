4b23997e435841873b3bc28ce017e61ef4b5ea62
Daniel Schwartz-Narbonne <danielsn@users.noreply.github.com>
2023-08-18 16:04:58 -0400

    refactor: Move profiler internal types to the internal module (#223)

--- profiling/src/profile/internal/mod.rs
+++ profiling/src/profile/internal/mod.rs
@@ -1,20 +1,26 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2023-Present Datadog, Inc.
 
+mod endpoints;
 mod function;
 mod label;
 mod location;
 mod mapping;
+mod sample;
 mod stack_trace;
 mod string;
+mod upscaling;
 mod value_type;
 
+pub use endpoints::*;
 pub use function::*;
 pub use label::*;
 pub use location::*;
 pub use mapping::*;
+pub use sample::*;
 pub use stack_trace::*;
 pub use string::*;
+pub use upscaling::*;
 pub use value_type::*;
 
 use super::pprof;
