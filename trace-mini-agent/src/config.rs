134f0cafbf0256fa6e8538f235a0ca9d40b358fc
David Lee <david.lee@datadoghq.com>
2023-09-01 10:27:10 -0700

    fix merge errs

--- trace-mini-agent/src/config.rs
+++ trace-mini-agent/src/config.rs
@@ -56,8 +56,7 @@ impl Config {
 
         let obfuscation_config = obfuscation_config::ObfuscationConfig::new().map_err(|err| {
             anyhow::anyhow!(
-                "Error creating obfuscation config, Mini Agent will not start. Error: {}",
-                err
+                "Error creating obfuscation config, Mini Agent will not start. Error: {err}",
             )
         })?;
 
