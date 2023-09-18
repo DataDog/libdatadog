c67cb9b84108747af652798e6a411693572cbb9f
David Lee <david.lee@datadoghq.com>
2023-09-18 08:30:59 -0700

    Update sql_tokenizer.rs

--- trace-obfuscation/src/sql_tokenizer.rs
+++ trace-obfuscation/src/sql_tokenizer.rs
@@ -669,7 +669,6 @@ impl SqlTokenizer {
                 token: format!("{delim}{delim}"),
             };
         }
-        self.get_advanced_chars();
         SqlTokenizerScanResult {
             token_kind: kind,
             token: s.to_string(),
