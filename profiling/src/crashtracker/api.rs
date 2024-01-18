90f6602bb6a814acd2a369c5afa3519bb9bf6a0d
Daniel Schwartz-Narbonne <daniel.schwartznarbonne@datadoghq.com>
2024-01-18 13:25:21 -0500

    Crashtracker only builds on unix for now

--- profiling/src/crashtracker/api.rs
+++ profiling/src/crashtracker/api.rs
@@ -1,5 +1,6 @@
 // Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2023-Present Datadog, Inc.
+#![cfg(unix)]
 
 use super::{
     counters::reset_counters,
