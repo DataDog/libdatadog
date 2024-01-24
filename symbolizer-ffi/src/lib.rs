3db0b809128476f52f63ccca51a7fb303c1782ce
r1viollet <erwan.viollet@datadoghq.com>
2024-01-24 10:13:22 +0100

    Blazesym APIs - minor comment

--- symbolizer-ffi/src/lib.rs
+++ symbolizer-ffi/src/lib.rs
@@ -1,5 +1,7 @@
 #[cfg(not(target_os = "windows"))]
 #[allow(non_camel_case_types)]
+// C types follow a different naming convention than Rust types, so we need to
+// allow snake_case here. Example: blaze_symbolizer
 mod symbolize;
 
 #[cfg(not(target_os = "windows"))]
