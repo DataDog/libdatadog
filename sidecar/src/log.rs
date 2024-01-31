1ce02721c7af76a674b859277cf9336ab53426bb
Bob Weinand <bob.weinand@datadoghq.com>
2024-01-31 16:55:08 +0100

    Add test for TemporarilyRetainedMap
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- sidecar/src/log.rs
+++ sidecar/src/log.rs
@@ -335,3 +335,63 @@ pub(crate) fn enable_logging() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+    use super::{TemporarilyRetainedKeyParser, TemporarilyRetainedMap};
+    use lazy_static::lazy_static;
+    use std::sync::atomic::{AtomicI32, Ordering};
+    use std::time::Duration;
+
+    lazy_static! {
+        static ref ENABLED: AtomicI32 = AtomicI32::default();
+        static ref DISABLED: AtomicI32 = AtomicI32::default();
+    }
+
+    impl TemporarilyRetainedKeyParser<i32> for String {
+        fn parse(&self) -> i32 {
+            str::parse(self).unwrap()
+        }
+
+        fn enable() {
+            ENABLED.fetch_add(1, Ordering::SeqCst);
+        }
+
+        fn disable() {
+            DISABLED.fetch_add(1, Ordering::SeqCst);
+        }
+    }
+
+    #[test]
+    fn test_refcounting_temporarily_retained_map() {
+        let map = TemporarilyRetainedMap::<_, i32> {
+            expire_after: Duration::from_millis(10),
+            ..Default::default()
+        };
+        let guard1 = map.add("1".to_string());
+        assert_eq!(1, *map.maps.read().unwrap().get("1").unwrap());
+        assert_eq!(1, ENABLED.load(Ordering::SeqCst));
+
+        drop(map.add("1".to_string()));
+
+        std::thread::sleep(Duration::from_millis(10));
+        let _guard2 = map.add("2".to_string());
+        // still there, even after drop of one occurrence
+        assert_eq!(1, *map.maps.read().unwrap().get("1").unwrap());
+
+        drop(guard1);
+        // Not immediately dropped
+        assert_eq!(1, *map.maps.read().unwrap().get("1").unwrap());
+
+        std::thread::sleep(Duration::from_millis(10));
+        // still there, drop should only happen after first insertion
+        assert_eq!(1, *map.maps.read().unwrap().get("1").unwrap());
+
+        drop(map.add("2".to_string()));
+        // actually dropped
+        assert_eq!(None, map.maps.read().unwrap().get("1"));
+
+        assert_eq!(1, DISABLED.load(Ordering::SeqCst));
+        assert_eq!(2, ENABLED.load(Ordering::SeqCst));
+    }
+}
