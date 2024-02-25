a21056a5fa3c0f54d555a978bd1f3ea9726ef0f8
Levi Morrison <levi.morrison@datadoghq.com>
2024-02-25 16:30:12 -0700

    simplify os/raw virtual {de,}alloc

--- profiling/src/alloc/virtual.rs
+++ profiling/src/alloc/virtual.rs
@@ -7,128 +7,91 @@ use std::io;
 
 #[cfg(unix)]
 mod unix {
+    use std::{io, ptr};
 
-    pub mod raw {
-        use std::{io, ptr};
+    /// Allocates virtual memory of the given size, which must be a
+    /// multiple of a page boundary and may not be zero.
+    pub fn virtual_alloc(size: usize) -> io::Result<ptr::NonNull<()>> {
+        let result = unsafe {
+            libc::mmap(
+                ptr::null_mut(),
+                size as libc::size_t,
+                libc::PROT_READ | libc::PROT_WRITE,
+                libc::MAP_PRIVATE | libc::MAP_ANON,
+                -1,
+                0,
+            )
+        };
 
-        /// Allocates virtual memory of the given size, which must be a
-        /// multiple of a page boundary and may not be zero.
-        pub fn virtual_alloc(size: usize) -> io::Result<ptr::NonNull<()>> {
-            let result = unsafe {
-                libc::mmap(
-                    ptr::null_mut(),
-                    size as libc::size_t,
-                    libc::PROT_READ | libc::PROT_WRITE,
-                    libc::MAP_PRIVATE | libc::MAP_ANON,
-                    -1,
-                    0,
-                )
-            };
-
-            if result == libc::MAP_FAILED {
-                return Err(io::Error::last_os_error());
-            }
-
-            if result.is_null() {
-                unsafe { libc::munmap(result, size as libc::size_t) };
-                Err(io::Error::new(
-                    io::ErrorKind::Other,
-                    "mmap returned a null pointer",
-                ))
-            } else {
-                // SAFETY: checked that the ptr was not null above.
-                Ok(unsafe { ptr::NonNull::new_unchecked(result).cast() })
-            }
+        if result == libc::MAP_FAILED {
+            return Err(io::Error::last_os_error());
         }
 
-        /// # Safety
-        ///  1. The ptr must have been previously allocated by [virtual_alloc].
-        ///  2. The size should be the exact size it was allocated with.
-        pub unsafe fn virtual_free(ptr: ptr::NonNull<()>, size: usize) -> io::Result<()> {
-            let result = libc::munmap(ptr.as_ptr().cast(), size as libc::size_t);
-            if result == -1 {
-                Err(io::Error::last_os_error())
-            } else {
-                Ok(())
-            }
+        if result.is_null() {
+            unsafe { libc::munmap(result, size as libc::size_t) };
+            Err(io::Error::new(
+                io::ErrorKind::Other,
+                "mmap returned a null pointer",
+            ))
+        } else {
+            // SAFETY: checked that the ptr was not null above.
+            Ok(unsafe { ptr::NonNull::new_unchecked(result).cast() })
+        }
+    }
+
+    /// # Safety
+    ///  1. The ptr must have been previously allocated by [virtual_alloc].
+    ///  2. The size should be the exact size it was allocated with.
+    pub unsafe fn virtual_free(ptr: ptr::NonNull<()>, size: usize) -> io::Result<()> {
+        let result = libc::munmap(ptr.as_ptr().cast(), size as libc::size_t);
+        if result == -1 {
+            Err(io::Error::last_os_error())
+        } else {
+            Ok(())
         }
     }
 }
 
 #[cfg(windows)]
 mod windows {
-    use std::io;
+    use std::{io, ptr};
     use windows_sys::Win32::System::Memory;
 
-    pub mod raw {
-        use super::*;
-        use core::ptr;
-
-        pub fn virtual_alloc(size: usize) -> io::Result<ptr::NonNull<()>> {
-            let result = unsafe {
-                Memory::VirtualAlloc(
-                    ptr::null(),
-                    size,
-                    Memory::MEM_COMMIT | Memory::MEM_RESERVE,
-                    Memory::PAGE_READWRITE,
-                )
-            };
-            if result.is_null() {
-                Err(io::Error::last_os_error())
-            } else {
-                // SAFETY: checked that the ptr was not null above.
-                Ok(unsafe { ptr::NonNull::new_unchecked(result).cast() })
-            }
-        }
-
-        /// # Safety
-        ///  1. The ptr must have been previously allocated by [crate::alloc::r#virtual::raw::virtual_alloc].
-        ///  2. The size should be the exact size it was allocated with.
-        pub unsafe fn virtual_free(ptr: ptr::NonNull<()>, size: usize) -> io::Result<()> {
-            let result = Memory::VirtualFree(ptr.as_ptr().cast(), 0, Memory::MEM_RELEASE);
-            if result == 0 {
-                Err(io::Error::last_os_error())
-            } else {
-                Ok(())
-            }
+    pub fn virtual_alloc(size: usize) -> io::Result<ptr::NonNull<()>> {
+        let result = unsafe {
+            Memory::VirtualAlloc(
+                ptr::null(),
+                size,
+                Memory::MEM_COMMIT | Memory::MEM_RESERVE,
+                Memory::PAGE_READWRITE,
+            )
+        };
+        if result.is_null() {
+            Err(io::Error::last_os_error())
+        } else {
+            // SAFETY: checked that the ptr was not null above.
+            Ok(unsafe { ptr::NonNull::new_unchecked(result).cast() })
         }
     }
 
-    pub fn alloc(min_size: usize) -> io::Result<Mapping> {
-        let page_size = page_size();
-        match pad_to(min_size, page_size) {
-            None => Err(io::Error::new(
-                io::ErrorKind::InvalidInput,
-                format!("requested virtual allocation {min_size} could not be padded to the page size {page_size}"),
-            )),
-            Some(size) => {
-                let result = unsafe {
-                    Memory::VirtualAlloc(
-                        ptr::null(),
-                        size,
-                        Memory::MEM_COMMIT | Memory::MEM_RESERVE,
-                        Memory::PAGE_READWRITE,
-                    )
-                };
-                if result.is_null() {
-                    Err(io::Error::last_os_error())
-                } else {
-                    Ok(Mapping {
-                        // SAFETY: checked that the ptr was not null above.
-                        base: unsafe { ptr::NonNull::new_unchecked(result).cast() },
-                        size,
-                    })
-                }
-            }
+    /// # Safety
+    ///  1. The ptr must have been previously allocated by [virtual_alloc].
+    ///  2. The size should be the exact size it was allocated with.
+    pub unsafe fn virtual_free(ptr: ptr::NonNull<()>, size: usize) -> io::Result<()> {
+        let result = Memory::VirtualFree(ptr.as_ptr().cast(), 0, Memory::MEM_RELEASE);
+        if result == 0 {
+            Err(io::Error::last_os_error())
+        } else {
+            Ok(())
         }
     }
 }
 
 #[cfg(unix)]
-pub use unix::*;
+use unix as os;
 
 #[cfg(windows)]
-pub use windows::*;
+use windows as os;
 
 #[repr(C)]
 pub struct Mapping {
@@ -149,7 +112,7 @@ impl Mapping {
                 format!("requested virtual allocation of {min_size} bytes could not be padded to the page size {page_size}"),
             )),
             Some(size) => Ok(Mapping {
-                base: raw::virtual_alloc(size)?,
+                base: os::virtual_alloc(size)?,
                 size,
             })
 
@@ -168,7 +131,7 @@ impl Mapping {
 impl Drop for Mapping {
     fn drop(&mut self) {
         // SAFETY: passing ptr and size exactly as received from alloc.
-        let _result = unsafe { raw::virtual_free(self.base, self.size) };
+        let _result = unsafe { os::virtual_free(self.base, self.size) };
 
         // If this fails, there's not much that can be done about it. It
         // could panic but panic in drops are generally frowned on.
