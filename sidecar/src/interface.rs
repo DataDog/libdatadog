dcdbe76848398e941605a1e51f599bf422a26663
Bob Weinand <bob.weinand@datadoghq.com>
2024-03-01 18:28:30 +0100

    Use bincode for much better performance (#336)
    Drop skip_serializing_if statements which are incompatible with non-self-describing formats like msgpack or bincode.
    
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- sidecar/src/interface.rs
+++ sidecar/src/interface.rs
@@ -31,7 +31,7 @@ use rand::Rng;
 use serde::{Deserialize, Serialize};
 use tokio::select;
 use tokio::task::{JoinError, JoinHandle};
-use tracing::{debug, enabled, error, info, Level};
+use tracing::{debug, enabled, error, info, warn, Level};
 
 use crate::agent_remote_config::AgentRemoteConfigWriter;
 use crate::config::get_product_endpoint;
@@ -96,20 +96,20 @@ pub enum RequestIdentifier {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SerializedTracerHeaderTags {
-    data: String,
+    data: Vec<u8>,
 }
 
 impl<'a> From<&'a SerializedTracerHeaderTags> for TracerHeaderTags<'a> {
     fn from(serialized: &'a SerializedTracerHeaderTags) -> Self {
-        // Panics if deserialization fails due (e.g. input containing double quote or backslash)
-        serde_json::from_str(&serialized.data).unwrap()
+        // Panics if deserialization fails (but that shouldn't ever happen)
+        bincode::deserialize(serialized.data.as_slice()).unwrap()
     }
 }
 
 impl<'a> From<TracerHeaderTags<'a>> for SerializedTracerHeaderTags {
     fn from(value: TracerHeaderTags<'a>) -> Self {
         SerializedTracerHeaderTags {
-            data: serde_json::to_string(&value).unwrap(),
+            data: bincode::serialize(&value).unwrap(),
         }
     }
 }
@@ -659,7 +659,10 @@ impl SidecarServer {
             }
         });
 
-        executor.await;
+        if let Err(e) = executor.await {
+            warn!("Error from executor: {e:?}");
+        }
+
         if let Ok((sessions, instances)) = session_interceptor.await {
             for session in sessions {
                 let stop = {
