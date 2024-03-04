0ba943f8af7345eae8932145284a627108d900a5
Daniel Schwartz-Narbonne <daniel.schwartznarbonne@datadoghq.com>
2024-03-04 14:57:28 -0500

    Better names and comments

--- profiling-ffi/src/crashtracker/crash_info.rs
+++ profiling-ffi/src/crashtracker/crash_info.rs
@@ -7,14 +7,14 @@ use crate::crashtracker::{
 use anyhow::Context;
 use ddcommon_ffi::{slice::AsBytes, CharSlice, Slice};
 
-use super::CrashtrackerMetadata;
+use super::{CrashtrackerConfiguration, CrashtrackerMetadata};
 
 /// Create a new crashinfo, and returns an opaque reference to it.
 /// # Safety
 /// No safety issues.
 #[no_mangle]
 #[must_use]
-pub unsafe extern "C" fn ddog_crashtracker_crashinfo_new() -> CrashInfoNewResult {
+pub unsafe extern "C" fn ddog_crashinfo_new() -> CrashInfoNewResult {
     CrashInfoNewResult::Ok(CrashInfo::new(datadog_crashtracker::CrashInfo::new()))
 }
 
@@ -28,17 +28,17 @@ pub unsafe extern "C" fn ddog_crashtracker_crashinfo_new() -> CrashInfoNewResult
 /// call.
 #[no_mangle]
 #[must_use]
-pub unsafe extern "C" fn ddog_crashtracker_crashinfo_add_counter(
+pub unsafe extern "C" fn ddog_crashinfo_add_counter(
     crashinfo: *mut CrashInfo,
     name: CharSlice,
     val: i64,
 ) -> CrashtrackerResult {
-    ddog_crashtracker_crashinfo_add_counter_impl(crashinfo, name, val)
-        .context("ddog_crashtracker_crashinfo_add_counter failed")
+    ddog_crashinfo_add_counter_impl(crashinfo, name, val)
+        .context("ddog_crashinfo_add_counter failed")
         .into()
 }
 
-unsafe fn ddog_crashtracker_crashinfo_add_counter_impl(
+unsafe fn ddog_crashinfo_add_counter_impl(
     crashinfo: *mut CrashInfo,
     name: CharSlice,
     val: i64,
@@ -58,16 +58,16 @@ unsafe fn ddog_crashtracker_crashinfo_add_counter_impl(
 /// call.
 #[no_mangle]
 #[must_use]
-pub unsafe extern "C" fn ddog_crashtracker_crashinfo_add_file(
+pub unsafe extern "C" fn ddog_crashinfo_add_file(
     crashinfo: *mut CrashInfo,
     name: CharSlice,
 ) -> CrashtrackerResult {
-    ddog_crashtracker_crashinfo_add_file_impl(crashinfo, name)
-        .context("ddog_crashtracker_crashinfo_add_file failed")
+    ddog_crashinfo_add_file_impl(crashinfo, name)
+        .context("ddog_crashinfo_add_file failed")
         .into()
 }
 
-unsafe fn ddog_crashtracker_crashinfo_add_file_impl(
+unsafe fn ddog_crashinfo_add_file_impl(
     crashinfo: *mut CrashInfo,
     name: CharSlice,
 ) -> anyhow::Result<()> {
@@ -77,25 +77,24 @@ unsafe fn ddog_crashtracker_crashinfo_add_file_impl(
     Ok(())
 }
 
-/// Adds the contents of "file" to the crashinfo
+/// Sets the crashinfo metadata
 ///
 /// # Safety
 /// `crashinfo` must be a valid pointer to a `CrashInfo` object.
-/// `name` should be a valid reference to a utf8 encoded String.
-/// The string is copied into the crashinfo, so it does not need to outlive this
-/// call.
+/// All references inside `metadata` must be valid.
+/// Strings are copied into the crashinfo, and do not need to outlive this call.
 #[no_mangle]
 #[must_use]
-pub unsafe extern "C" fn ddog_crashtracker_crashinfo_set_metadata(
+pub unsafe extern "C" fn ddog_crashinfo_set_metadata(
     crashinfo: *mut CrashInfo,
     metadata: CrashtrackerMetadata,
 ) -> CrashtrackerResult {
-    ddog_crashtracker_crashinfo_set_metadata_impl(crashinfo, metadata)
-        .context("ddog_crashtracker_crashinfo_set_metadata failed")
+    ddog_crashinfo_set_metadata_impl(crashinfo, metadata)
+        .context("ddog_crashinfo_set_metadata failed")
         .into()
 }
 
-unsafe fn ddog_crashtracker_crashinfo_set_metadata_impl(
+unsafe fn ddog_crashinfo_set_metadata_impl(
     crashinfo: *mut CrashInfo,
     metadata: CrashtrackerMetadata,
 ) -> anyhow::Result<()> {
@@ -105,25 +104,24 @@ unsafe fn ddog_crashtracker_crashinfo_set_metadata_impl(
     Ok(())
 }
 
-/// Adds the contents of "file" to the crashinfo
+/// Sets `stacktrace` as the default stacktrace on `crashinfo`.
 ///
 /// # Safety
 /// `crashinfo` must be a valid pointer to a `CrashInfo` object.
-/// `name` should be a valid reference to a utf8 encoded String.
-/// The string is copied into the crashinfo, so it does not need to outlive this
-/// call.
+/// All references inside `stacktraces` must be valid.
+/// Strings are copied into the crashinfo, and do not need to outlive this call.
 #[no_mangle]
 #[must_use]
-pub unsafe extern "C" fn ddog_crashtracker_crashinfo_set_stacktrace(
+pub unsafe extern "C" fn ddog_crashinfo_set_stacktrace(
     crashinfo: *mut CrashInfo,
     stacktrace: Slice<StackFrame>,
 ) -> CrashtrackerResult {
-    ddog_crashtracker_crashinfo_set_stacktrace_impl(crashinfo, stacktrace)
-        .context("ddog_crashtracker_crashinfo_set_metadata failed")
+    ddog_crashinfo_set_stacktrace_impl(crashinfo, stacktrace)
+        .context("ddog_crashinfo_set_metadata failed")
         .into()
 }
 
-unsafe fn ddog_crashtracker_crashinfo_set_stacktrace_impl(
+unsafe fn ddog_crashinfo_set_stacktrace_impl(
     crashinfo: *mut CrashInfo,
     stacktrace: Slice<StackFrame>,
 ) -> anyhow::Result<()> {
@@ -133,5 +131,56 @@ unsafe fn ddog_crashtracker_crashinfo_set_stacktrace_impl(
         stacktrace_vec.push(s.try_into()?)
     }
     crashinfo.set_stacktrace(stacktrace_vec)?;
+    Ok(())
+}
+
+/// Exports `crashinfo` to the Instrumentation Telemetry backend
+///
+/// # Safety
+/// `crashinfo` must be a valid pointer to a `CrashInfo` object.
+#[no_mangle]
+#[must_use]
+pub unsafe extern "C" fn ddog_crashinfo_upload_to_telemetry(
+    crashinfo: *mut CrashInfo,
+    config: CrashtrackerConfiguration,
+) -> CrashtrackerResult {
+    ddog_crashinfo_upload_to_telemetry_impl(crashinfo, config)
+        .context("ddog_crashinfo_upload_to_telemetry failed")
+        .into()
+}
+
+unsafe fn ddog_crashinfo_upload_to_telemetry_impl(
+    crashinfo: *mut CrashInfo,
+    config: CrashtrackerConfiguration,
+) -> anyhow::Result<()> {
+    let crashinfo = crashinfo_ptr_to_inner(crashinfo)?;
+    let config = config.try_into()?;
+    crashinfo.upload_to_telemetry(&config)?;
+    Ok(())
+}
+
+/// Exports `crashinfo` to the profiling backend at `endpoint`
+/// Note that we support the "file://" endpoint for local file output.
+/// # Safety
+/// `crashinfo` must be a valid pointer to a `CrashInfo` object.
+#[no_mangle]
+#[must_use]
+pub unsafe extern "C" fn ddog_crashinfo_upload_to_endpoint(
+    crashinfo: *mut CrashInfo,
+    config: CrashtrackerConfiguration,
+) -> CrashtrackerResult {
+    ddog_crashinfo_upload_to_endpoint_impl(crashinfo, config)
+        .context("ddog_crashinfo_upload_to_endpoint failed")
+        .into()
+}
+
+unsafe fn ddog_crashinfo_upload_to_endpoint_impl(
+    crashinfo: *mut CrashInfo,
+    config: CrashtrackerConfiguration,
+) -> anyhow::Result<()> {
+    let crashinfo = crashinfo_ptr_to_inner(crashinfo)?;
+    let config: datadog_crashtracker::CrashtrackerConfiguration = config.try_into()?;
+    let endpoint = config.endpoint.context("Expected endpoint")?;
+    crashinfo.upload_to_endpoint(endpoint, config.timeout)?;
     Ok(())
 }
