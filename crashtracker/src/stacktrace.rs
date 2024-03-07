5c36a368f691a16aca764ca324517ff146c92701
Daniel Schwartz-Narbonne <daniel.schwartznarbonne@datadoghq.com>
2024-03-07 15:41:52 -0500

    More unix removal

--- crashtracker/src/stacktrace.rs
+++ crashtracker/src/stacktrace.rs
@@ -1,21 +1,7 @@
 // Copyright 2024-Present Datadog, Inc. https://www.datadoghq.com/
 // SPDX-License-Identifier: Apache-2.0
 
-use blazesym::symbolize::{Input, Source, Sym, Symbolized, Symbolizer};
 use serde::{Deserialize, Serialize};
-
-impl From<Sym<'_>> for StackFrameNames {
-    fn from(value: Sym) -> Self {
-        let mut rval = Self::default();
-        if let Some(c) = value.code_info {
-            rval.lineno = c.line;
-            rval.filename = Some(c.to_path().display().to_string());
-            rval.colno = c.column.map(|c| c.into());
-        }
-        rval.name = Some(value.name.into_owned());
-        rval
-    }
-}
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Default)]
 pub struct StackFrameNames {
@@ -35,22 +21,48 @@ pub struct StackFrame {
     pub symbol_address: Option<String>,
 }
 
-impl StackFrame {
-    pub fn resolve_names(&mut self, src: &Source, symbolizer: &Symbolizer) -> anyhow::Result<()> {
-        if let Some(ip) = &self.ip {
-            let ip = ip.trim_start_matches("0x");
-            let ip = u64::from_str_radix(ip, 16)?;
-            let input = Input::AbsAddr(ip);
-            match symbolizer.symbolize_single(src, input)? {
-                Symbolized::Sym(s) => {
-                    //TODO: handle
-                    self.names = Some(vec![s.into()]);
-                }
-                Symbolized::Unknown(reason) => {
-                    anyhow::bail!("Couldn't symbolize {ip}: {reason}");
+#[cfg(unix)]
+pub use unix::*;
+
+#[cfg(unix)]
+mod unix {
+    use super::*;
+    use blazesym::symbolize::{Input, Source, Sym, Symbolized, Symbolizer};
+
+    impl From<Sym<'_>> for StackFrameNames {
+        fn from(value: Sym) -> Self {
+            let mut rval = Self::default();
+            if let Some(c) = value.code_info {
+                rval.lineno = c.line;
+                rval.filename = Some(c.to_path().display().to_string());
+                rval.colno = c.column.map(|c| c.into());
+            }
+            rval.name = Some(value.name.into_owned());
+            rval
+        }
+    }
+
+    impl StackFrame {
+        pub fn resolve_names(
+            &mut self,
+            src: &Source,
+            symbolizer: &Symbolizer,
+        ) -> anyhow::Result<()> {
+            if let Some(ip) = &self.ip {
+                let ip = ip.trim_start_matches("0x");
+                let ip = u64::from_str_radix(ip, 16)?;
+                let input = Input::AbsAddr(ip);
+                match symbolizer.symbolize_single(src, input)? {
+                    Symbolized::Sym(s) => {
+                        //TODO: handle
+                        self.names = Some(vec![s.into()]);
+                    }
+                    Symbolized::Unknown(reason) => {
+                        anyhow::bail!("Couldn't symbolize {ip}: {reason}");
+                    }
                 }
             }
+            Ok(())
         }
-        Ok(())
     }
 }
