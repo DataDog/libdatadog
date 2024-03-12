faee26419bc1c6fea86d37df31d9b32c5cacf417
Julio Gonzalez <julio.gonzalez@datadoghq.com>
2024-03-12 14:33:27 +0100

    [data-pipeline] Add FFI to trace exporter

--- data-pipeline-ffi/src/trace_exporter.rs
+++ data-pipeline-ffi/src/trace_exporter.rs
@@ -1,54 +1,59 @@
-use std::ffi::{CStr, CString, c_char};
+// Copyright 2024-Present Datadog, Inc. https://www.datadoghq.com/
+// SPDX-License-Identifier: Apache-2.0
+
+use bytes::Bytes;
 use data_pipeline::trace_exporter::TraceExporter;
 use data_pipeline::trace_exporter::TraceExporterBuilder;
-
-#[repr(C)]
-pub struct ExporterHandle {
-    _exporter: Box<TraceExporter>
-}
+use ddcommon_ffi::{
+    slice::{AsBytes, ByteSlice},
+    CharSlice,
+};
+use std::ffi::c_char;
 
 #[no_mangle]
-pub unsafe extern "C" fn dd_trace_exporter_new(
-    host: *const c_char,
+pub unsafe extern "C" fn ddog_trace_exporter_new(
+    host: CharSlice,
     port: u16,
-    timeout: u64,
-    tracer_version: *const c_char,
-    language: *const c_char,
-    language_version: *const c_char,
-    language_interpreter: *const c_char) -> *mut ExporterHandle {
-
+    tracer_version: CharSlice,
+    language: CharSlice,
+    language_version: CharSlice,
+    language_interpreter: CharSlice,
+) -> *mut TraceExporter {
     let mut builder = TraceExporterBuilder::default();
 
-    let handle = ExporterHandle {
-        _exporter: Box::new(builder
-                            .set_timeout(timeout)
-                            .set_host(CStr::from_ptr(host).to_str().unwrap())
-                            .set_port(port)
-                            .set_tracer_version(CStr::from_ptr(tracer_version).to_str().unwrap())
-                            .set_language(CStr::from_ptr(language).to_str().unwrap())
-                            .set_language_version(CStr::from_ptr(language_version).to_str().unwrap())
-                            .set_language_interpreter(CStr::from_ptr(language_interpreter).to_str().unwrap())
-                            .build())
-    };
+    let exporter = builder
+        .set_host(host.to_utf8_lossy().as_ref())
+        .set_port(port)
+        .set_tracer_version(tracer_version.to_utf8_lossy().as_ref())
+        .set_language(language.to_utf8_lossy().as_ref())
+        .set_language_version(language_version.to_utf8_lossy().as_ref())
+        .set_language_interpreter(language_interpreter.to_utf8_lossy().as_ref())
+        .build()
+        .unwrap();
 
-    Box::into_raw(Box::new(handle))
+    Box::into_raw(Box::new(exporter))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn dd_trace_exporter_free(ctx: *mut ExporterHandle) {
-    drop(Box::from_raw(ctx))
+pub unsafe extern "C" fn ddog_trace_exporter_free(ctx: *mut TraceExporter) {
+    if let Some(p) = ctx.as_mut() {
+        drop(Box::from_raw(p))
+    }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn dd_trace_exporter_send(
-    ctx: *mut ExporterHandle,
-    trace: *const u8,
-    size: usize,
-    trace_count: usize) -> *const c_char {
+pub unsafe extern "C" fn ddog_trace_exporter_send(
+    ctx: *mut TraceExporter,
+    trace: ByteSlice,
+    trace_count: usize,
+) -> *mut c_char {
+    let handle = Box::from_raw(ctx);
+    let response = handle
+        .send(Bytes::copy_from_slice(trace.as_bytes()), trace_count)
+        .unwrap_or(String::from(""));
 
-    let mut handle = Box::from_raw(ctx);
-    let response = handle._exporter.send(std::slice::from_raw_parts(trace, size), trace_count).unwrap();
-
-    CString::new(response).unwrap().into_raw()
+    let ret = libc::malloc(response.len() + 1);
+    std::ptr::copy(response.as_bytes().as_ptr(), ret.cast(), response.len());
+    std::ptr::write(ret.add(response.len()) as *mut u8, 0u8);
+    ret as *mut c_char
 }
-
