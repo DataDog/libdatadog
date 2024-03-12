0c23c1fe2270baf44a3ca2f85a360072a96c0f70
Julio Gonzalez <julio.gonzalez@datadoghq.com>
2024-03-12 14:33:27 +0100

    Upload dummy implementations.

--- data-pipeline-ffi/src/trace_exporter.rs
+++ data-pipeline-ffi/src/trace_exporter.rs
@@ -1,11 +1,8 @@
-use std::ffi::{CStr, CString, c_char};
-use data_pipeline::trace_exporter::TraceExporter;
-use data_pipeline::trace_exporter::TraceExporterBuilder;
+use std::ffi::{CString, c_char};
+// use bytes::Bytes;
+// use data_pipeline::trace_exporter::TraceExporter;
 
-#[repr(C)]
-pub struct ExporterHandle {
-    _exporter: Box<TraceExporter>
-}
+pub struct TraceExporter;
 
 #[no_mangle]
 pub unsafe extern "C" fn dd_trace_exporter_new(
@@ -15,40 +12,25 @@ pub unsafe extern "C" fn dd_trace_exporter_new(
     tracer_version: *const c_char,
     language: *const c_char,
     language_version: *const c_char,
-    language_interpreter: *const c_char) -> *mut ExporterHandle {
+    language_interpreter: *const c_char) -> *mut TraceExporter {
 
-    let mut builder = TraceExporterBuilder::default();
-
-    let handle = ExporterHandle {
-        _exporter: Box::new(builder
-                            .set_timeout(timeout)
-                            .set_host(CStr::from_ptr(host).to_str().unwrap())
-                            .set_port(port)
-                            .set_tracer_version(CStr::from_ptr(tracer_version).to_str().unwrap())
-                            .set_language(CStr::from_ptr(language).to_str().unwrap())
-                            .set_language_version(CStr::from_ptr(language_version).to_str().unwrap())
-                            .set_language_interpreter(CStr::from_ptr(language_interpreter).to_str().unwrap())
-                            .build())
-    };
-
-    Box::into_raw(Box::new(handle))
+    Box::into_raw(Box::new(TraceExporter{}))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn dd_trace_exporter_free(ctx: *mut ExporterHandle) {
-    drop(Box::from_raw(ctx))
+pub unsafe extern "C" fn dd_trace_exporter_free(ctx: *mut TraceExporter) {
+    if !ctx.is_null() {
+        drop(Box::from_raw(ctx))
+    }
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn dd_trace_exporter_send(
-    ctx: *mut ExporterHandle,
+    ctx: *mut TraceExporter,
     trace: *const u8,
     size: usize,
     trace_count: usize) -> *const c_char {
 
-    let mut handle = Box::from_raw(ctx);
-    let response = handle._exporter.send(std::slice::from_raw_parts(trace, size), trace_count).unwrap();
-
-    CString::new(response).unwrap().into_raw()
+    CString::new(String::from("Response")).unwrap().into_raw()
 }
 
