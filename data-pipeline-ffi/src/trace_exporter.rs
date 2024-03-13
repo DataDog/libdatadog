29cb386f6e9fc1f4303e67c880b28e89ef838c86
Julio Gonzalez <julio.gonzalez@datadoghq.com>
2024-03-13 09:06:14 +0100

    Add license

--- data-pipeline-ffi/src/trace_exporter.rs
+++ data-pipeline-ffi/src/trace_exporter.rs
@@ -1,3 +1,6 @@
+// Copyright 2024-Present Datadog, Inc. https://www.datadoghq.com/
+// SPDX-License-Identifier: Apache-2.0
+
 use bytes::Bytes;
 use data_pipeline::trace_exporter::TraceExporter;
 use data_pipeline::trace_exporter::TraceExporterBuilder;
