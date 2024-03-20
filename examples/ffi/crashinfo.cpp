5c311b9837131747d2285021c2b1e1b772067666
Daniel Schwartz-Narbonne <daniel.schwartznarbonne@datadoghq.com>
2024-03-19 20:48:16 -0400

    Example adding stacktrace

--- examples/ffi/crashinfo.cpp
+++ examples/ffi/crashinfo.cpp
@@ -13,7 +13,9 @@ extern "C" {
 #include <thread>
 
 static ddog_CharSlice to_slice_c_char(const char *s) { return {.ptr = s, .len = strlen(s)}; }
-
+static ddog_CharSlice to_slice_string(std::string &s) {
+  return {.ptr = s.data(), .len = s.length()};
+}
 struct Deleter {
   void operator()(ddog_prof_CrashInfo *object) { ddog_crashinfo_drop(object); }
 };
@@ -29,6 +31,39 @@ void check_result(ddog_prof_CrashtrackerResult result, const char *msg) {
     ddog_Error_drop(&result.err);
     exit(EXIT_FAILURE);
   }
+}
+
+void add_stacktrace(std::unique_ptr<ddog_prof_CrashInfo, Deleter> &crashinfo) {
+
+  // Collect things into vectors so they stay alive till the function exits
+  std::vector<std::string> filenames;
+  std::vector<std::string> function_names;
+  for (uintptr_t i = 0; i < 20; ++i) {
+    filenames.push_back("/path/to/code/file_" + std::to_string(i));
+    function_names.push_back("func_" + std::to_string(i));
+  }
+
+  std::vector<ddog_prof_StackFrameNames> names;
+  for (uintptr_t i = 0; i < 20; ++i) {
+    names.push_back({.colno = {.tag = DDOG_PROF_OPTION_U32_NONE_U32},
+                     .filename = to_slice_string(filenames[i]),
+                     .lineno = {.tag = DDOG_PROF_OPTION_U32_NONE_U32},
+                     .name = to_slice_string(function_names[i])});
+  }
+
+  std::vector<ddog_prof_StackFrame> trace;
+  for (uintptr_t i = 0; i < 20; ++i) {
+    ddog_prof_StackFrame frame = {.ip = i,
+                                  .module_base_address = 0,
+                                  .names = {.ptr = &names[i], .len = 1},
+                                  .sp = 0,
+                                  .symbol_address = 0};
+    trace.push_back(frame);
+  }
+  ddog_prof_Slice_StackFrame trace_slice = {.ptr = trace.data(), .len = trace.size()};
+
+  check_result(ddog_crashinfo_set_stacktrace(crashinfo.get(), to_slice_c_char(""), trace_slice),
+               "Failed to set stacktrace");
 }
 
 int main(void) {
@@ -54,6 +89,15 @@ int main(void) {
   };
 
   check_result(ddog_crashinfo_set_metadata(crashinfo.get(), metadata), "Failed to add metadata");
+
+  // This API allows one to capture useful files (e.g. /proc/pid/maps)
+  // For testing purposes, use `/etc/hosts` which should exist on any reasonable
+  // UNIX system
+  check_result(ddog_crashinfo_add_file(crashinfo.get(), to_slice_c_char("/etc/hosts")),
+               "Failed to add file");
+
+  add_stacktrace(crashinfo);
+
   auto endpoint = ddog_Endpoint_file(to_slice_c_char("file://tmp/test.txt"));
   check_result(ddog_crashinfo_upload_to_endpoint(crashinfo.get(), endpoint, 1),
                "Failed to export to file");
