59314c1049ac9fa7f70f17e5200cc26daea0e1ae
David Sanchez <838104+sanchda@users.noreply.github.com>
2024-04-09 17:02:27 -0500

    Use correct type for comparison in crashtracker eg (#382)
    Co-authored-by: sanchda <sanchda@users.noreply.github.com>

--- crashtracker/libdatadog-crashtracking-receiver.c
+++ crashtracker/libdatadog-crashtracking-receiver.c
@@ -8,7 +8,7 @@
 
 int main(void) {
   ddog_prof_CrashtrackerResult new_result = ddog_prof_Crashtracker_receiver_entry_point();
-  if (new_result.tag != DDOG_PROF_PROFILE_NEW_RESULT_OK) {
+  if (new_result.tag != DDOG_PROF_CRASHTRACKER_RESULT_OK) {
     ddog_CharSlice message = ddog_Error_message(&new_result.err);
     fprintf(stderr, "%*s", (int)message.len, message.ptr);
     ddog_Error_drop(&new_result.err);
