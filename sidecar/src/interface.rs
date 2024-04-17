43dff048d8715960cc0d0512ceb6e9f4b0be7268
Edmund Kump <edmund.kump@datadoghq.com>
2024-04-17 11:58:01 -0400

    refactor sidecar_interface from interface.rs to separate file

--- sidecar/src/interface.rs
+++ sidecar/src/interface.rs
@@ -57,42 +57,9 @@ use crate::{config, log, tracer};
 
 use crate::service::{
     InstanceId, QueueId, RequestIdentification, RequestIdentifier, RuntimeMetadata,
-    SerializedTracerHeaderTags,
+    SerializedTracerHeaderTags, SidecarInterface, SidecarInterfaceRequest,
+    SidecarInterfaceResponse,
 };
-
-#[datadog_sidecar_macros::extract_request_id]
-#[datadog_ipc_macros::impl_transfer_handles]
-#[tarpc::service]
-pub trait SidecarInterface {
-    async fn enqueue_actions(
-        instance_id: InstanceId,
-        queue_id: QueueId,
-        actions: Vec<SidecarAction>,
-    );
-    async fn register_service_and_flush_queued_actions(
-        instance_id: InstanceId,
-        queue_id: QueueId,
-        meta: RuntimeMetadata,
-        service_name: String,
-        env_name: String,
-    );
-    async fn set_session_config(session_id: String, config: SessionConfig);
-    async fn shutdown_runtime(instance_id: InstanceId);
-    async fn shutdown_session(session_id: String);
-    async fn send_trace_v04_shm(
-        instance_id: InstanceId,
-        #[SerializedHandle] handle: ShmHandle,
-        headers: SerializedTracerHeaderTags,
-    );
-    async fn send_trace_v04_bytes(
-        instance_id: InstanceId,
-        data: Vec<u8>,
-        headers: SerializedTracerHeaderTags,
-    );
-    async fn ping();
-    async fn dump() -> String;
-    async fn stats() -> String;
-}
 
 #[derive(Serialize, Deserialize)]
 pub struct SidecarStats {
--- sidecar/src/service/mod.rs
+++ sidecar/src/service/mod.rs
@@ -6,9 +6,13 @@ pub use queue_id::QueueId;
 pub use request_identification::{RequestIdentification, RequestIdentifier};
 pub use runtime_metadata::RuntimeMetadata;
 pub use serialized_tracer_header_tags::SerializedTracerHeaderTags;
+pub use sidecar_interface::{
+    SidecarInterface, SidecarInterfaceClient, SidecarInterfaceRequest, SidecarInterfaceResponse,
+};
 
 mod instance_id;
 pub mod queue_id;
 mod request_identification;
 mod runtime_metadata;
 mod serialized_tracer_header_tags;
+mod sidecar_interface;
