1ab01f8c1bf8c2741c2763df4574a912e9363aad
Edmund Kump <edmund.kump@datadoghq.com>
2024-05-21 16:17:57 -0400

    Refactor send function into smaller functions

--- trace-utils/src/send_data.rs
+++ trace-utils/src/send_data.rs
@@ -135,12 +135,25 @@ impl SendData {
             headers,
         }
     }
-
     pub async fn send<'a>(self) -> SendDataResult {
         let target = &self.target;
 
+        let req = self.create_request_builder();
+
+        if self.use_protobuf() {
+            self.send_with_protobuf(req).await
+        } else {
+            self.send_with_msgpack(req).await
+        }
+    }
+    
+    fn use_protobuf(&self) -> bool {
+        self.target.api_key.is_some()
+    }
+
+    fn create_request_builder(&self) -> HttpRequestBuilder {
         let mut req = hyper::Request::builder()
-            .uri(target.url.clone())
+            .uri(self.target.url.clone())
             .header(
                 hyper::header::USER_AGENT,
                 concat!("Tracer/", env!("CARGO_PKG_VERSION")),
@@ -151,78 +164,86 @@ impl SendData {
             req = req.header(*key, value);
         }
 
-        async fn send_request(
-            req: HttpRequestBuilder,
-            payload: Vec<u8>,
-        ) -> Result<Response<Body>, SendRequestError> {
-            let req = req
-                .body(Body::from(payload))
-                .map_err(|e| SendRequestError::Any(anyhow!(e)))?;
+        req
+    }
 
-            Client::builder()
-                .build(connector::Connector::default())
-                .request(req)
-                .await
-                .map_err(SendRequestError::Hyper)
-        }
+    async fn send_request(
+        &self,
+        req: HttpRequestBuilder,
+        payload: Vec<u8>,
+    ) -> Result<Response<Body>, SendRequestError> {
+        let req = req
+            .body(Body::from(payload))
+            .map_err(|e| SendRequestError::Any(anyhow!(e)))?;
 
+        Client::builder()
+            .build(connector::Connector::default())
+            .request(req)
+            .await
+            .map_err(SendRequestError::Hyper)
+    }
+
+    async fn send_with_protobuf(&self, mut req: HttpRequestBuilder) -> SendDataResult {
         let mut result = SendDataResult::new();
 
-        if target.api_key.is_some() {
-            req = req.header("Content-type", "application/x-protobuf");
+        req = req.header("Content-type", "application/x-protobuf");
 
-            let agent_payload = construct_agent_payload(self.tracer_payloads);
-            let serialized_trace_payload = match serialize_proto_payload(&agent_payload)
-                .context("Failed to serialize trace agent payload, dropping traces")
-            {
+        let agent_payload = construct_agent_payload(self.tracer_payloads.clone());
+        let serialized_trace_payload = match serialize_proto_payload(&agent_payload)
+            .context("Failed to serialize trace agent payload, dropping traces")
+        {
+            Ok(p) => p,
+            Err(e) => return result.error(e),
+        };
+
+        result
+            .update(
+                self.send_request(req, serialized_trace_payload).await,
+                StatusCode::ACCEPTED,
+            )
+            .await;
+
+        result
+    }
+
+    async fn send_with_msgpack(&self, mut req: HttpRequestBuilder) -> SendDataResult {
+        let mut result = SendDataResult::new();
+
+        req = req.header("Content-type", "application/msgpack");
+
+        let (template, _) = req.body(()).unwrap().into_parts();
+
+        let mut futures = FuturesUnordered::new();
+        for tracer_payload in self.tracer_payloads.clone().into_iter() {
+            let mut builder = HttpRequestBuilder::new()
+                .method(template.method.clone())
+                .uri(template.uri.clone())
+                .version(template.version)
+                .header(
+                    "X-Datadog-Trace-Count",
+                    tracer_payload.chunks.len().to_string(),
+                );
+            builder
+                .headers_mut()
+                .unwrap()
+                .extend(template.headers.clone());
+
+            let payload = match rmp_serde::to_vec_named(&tracer_payload) {
                 Ok(p) => p,
-                Err(e) => return result.error(e),
+                Err(e) => return result.error(anyhow!(e)),
             };
 
-            result
-                .update(
-                    send_request(req, serialized_trace_payload).await,
-                    StatusCode::ACCEPTED,
-                )
-                .await;
-            result
-        } else {
-            req = req.header("Content-type", "application/msgpack");
-
-            let (template, _) = req.body(()).unwrap().into_parts();
-
-            let mut futures = FuturesUnordered::new();
-            for tracer_payload in self.tracer_payloads.into_iter() {
-                let mut builder = HttpRequestBuilder::new()
-                    .method(template.method.clone())
-                    .uri(template.uri.clone())
-                    .version(template.version)
-                    .header(
-                        "X-Datadog-Trace-Count",
-                        tracer_payload.chunks.len().to_string(),
-                    );
-                builder
-                    .headers_mut()
-                    .unwrap()
-                    .extend(template.headers.clone());
-
-                let payload = match rmp_serde::to_vec_named(&tracer_payload) {
-                    Ok(p) => p,
-                    Err(e) => return result.error(anyhow!(e)),
-                };
-
-                futures.push(send_request(builder, payload));
-            }
-            loop {
-                match futures.next().await {
-                    Some(response) => {
-                        result.update(response, StatusCode::OK).await;
-                        if result.last_result.is_err() {
-                            return result;
-                        }
+            futures.push(self.send_request(builder, payload));
+        }
+        loop {
+            match futures.next().await {
+                Some(response) => {
+                    result.update(response, StatusCode::OK).await;
+                    if result.last_result.is_err() {
+                        return result;
                     }
-                    None => return result,
                 }
+                None => return result,
             }
         }
     }
