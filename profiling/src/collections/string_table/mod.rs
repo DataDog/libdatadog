edc1524f7e8ac2648d9c98e9729e8de88c38f76b
Daniel Schwartz-Narbonne <daniel.schwartznarbonne@datadoghq.com>
2024-05-21 11:07:39 -0400

    cleaner comment explaining string lengths

--- profiling/src/collections/string_table/mod.rs
+++ profiling/src/collections/string_table/mod.rs
@@ -238,8 +238,9 @@ mod tests {
     /// as well as functional correctness (the table should behave like an
     /// ordered set).  
     /// Limitations:
-    ///   - The length of strings appears to be limited: `assert!(string.len() < 50);` failed, but
-    ///     `100` passed (in 10s)
+    ///   - The crate used here to generate Strings internally has a default range for the length of a string, (0..=64)
+    ///     We should experiment with longer strings to see what happens.
+    ///     https://github.com/camshaft/bolero/blob/f401669697ffcbe7f34cbfd09fd57b93d5df734c/lib/bolero-generator/src/alloc/mod.rs#L17
     ///   - Since iterating is destructive, can only check the string values once.
     /// `cargo +nightly bolero test collections::string_table::tests::fuzz_string_table -T 1min`
     #[test]
