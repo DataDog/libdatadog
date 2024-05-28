d8a3c9aece58631c323601449c705f9c1d784de4
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-05-28 13:11:59 -0400

    [profiling] use internal Label instead of pprof::Label (#454)
    * use internal Label instead of pprof::Label
    
    * clippy and fmt
    
    * remove StringId::new

--- profiling/src/collections/identifiable/string_id.rs
+++ profiling/src/collections/identifiable/string_id.rs
@@ -15,15 +15,6 @@ impl StringId {
         Self(0)
     }
 
-    // todo: remove when upscaling uses internal::* instead of pprof::*
-    pub fn new<T>(v: T) -> Self
-    where
-        T: TryInto<u32>,
-        T::Error: core::fmt::Debug,
-    {
-        Self(v.try_into().expect("StringId to fit into a u32"))
-    }
-
     #[inline]
     pub const fn is_zero(&self) -> bool {
         self.0 == 0
@@ -39,7 +30,7 @@ impl Id for StringId {
     type RawId = i64;
 
     fn from_offset(inner: usize) -> Self {
-        Self::new(inner)
+        Self(inner.try_into().expect("StringId to fit into a u32"))
     }
 
     fn to_raw_id(&self) -> Self::RawId {
