f2fac4d60674d08b43a50e78a31c23c1fbf6c5fa
Bob Weinand <bob.weinand@datadoghq.com>
2024-07-12 20:47:54 +0200

    Buffer debugger data sending
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- live-debugger/src/sender.rs
+++ live-debugger/src/sender.rs
@@ -6,13 +6,17 @@ use ddcommon::connector::Connector;
 use ddcommon::tag::Tag;
 use ddcommon::Endpoint;
 use hyper::http::uri::PathAndQuery;
-use hyper::{Body, Client, Method, Uri};
+use hyper::{Body, Client, Method, Response, Uri};
 use percent_encoding::{percent_encode, CONTROLS};
 use serde::{Deserialize, Serialize};
 use std::fmt::Display;
 use std::hash::Hash;
 use std::str::FromStr;
+use hyper::body::{Bytes, Sender};
 use uuid::Uuid;
+use constcat::concat;
+use hyper::client::ResponseFuture;
+use tokio::task::JoinHandle;
 
 pub const PROD_LOGS_INTAKE_SUBDOMAIN: &str = "http-intake.logs";
 pub const PROD_DIAGNOSTICS_INTAKE_SUBDOMAIN: &str = "debugger-intake";
@@ -96,85 +100,138 @@ pub fn generate_tags(
     percent_encode(tags.as_bytes(), CONTROLS).to_string()
 }
 
+#[derive(Debug, Default)]
+enum SenderFuture {
+    #[default]
+    Error,
+    Outstanding(ResponseFuture),
+    Submitted(JoinHandle<hyper::Result<Response<Body>>>),
+}
+
+pub struct PayloadSender {
+    future: SenderFuture,
+    sender: Sender,
+    needs_boundary: bool,
+    payloads: u32,
+}
+
+const BOUNDARY: &str = "------------------------44617461646f67";
+const BOUNDARY_LINE: &str = concat!("--", BOUNDARY, "\r\n");
+
+impl PayloadSender {
+    pub fn new(config: &Config, debugger_type: DebuggerType, percent_encoded_tags: &str) -> anyhow::Result<Self> {
+        let endpoint = match debugger_type {
+            DebuggerType::Diagnostics => &config.diagnostics_endpoint,
+            DebuggerType::Logs => &config.logs_endpoint,
+        }.as_ref().unwrap();
+
+        let mut url = endpoint.url.clone();
+        let mut parts = url.into_parts();
+        let query = format!(
+            "{}?ddtags={}",
+            parts.path_and_query.unwrap(),
+            percent_encoded_tags
+        );
+        parts.path_and_query = Some(PathAndQuery::from_str(&query)?);
+        url = Uri::from_parts(parts)?;
+
+        let mut req = hyper::Request::builder()
+            .header(
+                hyper::header::USER_AGENT,
+                concat!("Tracer/", env!("CARGO_PKG_VERSION")),
+            )
+            .method(Method::POST)
+            .uri(url);
+
+        if endpoint.api_key.is_some() {
+            req = req.header("DD-EVP-ORIGIN", "agent-debugger");
+        }
+
+        let (sender, body) = Body::channel();
+
+        let needs_boundary = debugger_type == DebuggerType::Diagnostics;
+        let req = req.header("Content-type", if needs_boundary {
+            concat!("multipart/form-data; boundary=", BOUNDARY)
+        } else {
+            "application/json"
+        });
+
+        let future = Client::builder().build(Connector::default()).request(req.body(body)?);
+        Ok(PayloadSender { future: SenderFuture::Outstanding(future), sender, needs_boundary, payloads: 0 })
+    }
+
+    pub async fn append(&mut self, data: &[u8]) -> anyhow::Result<()> {
+        let first = match std::mem::take(&mut self.future) {
+            SenderFuture::Outstanding(future) => {
+                if self.needs_boundary {
+                    let header = concat!(
+                        BOUNDARY_LINE,
+                        "Content-Disposition: form-data; name=\"event\"; filename=\"event.json\"\r\n",
+                        "Content-Type: application/json\r\n",
+                        "\r\n",
+                    );
+                    self.sender.send_data(header.into()).await?;
+                }
+    
+                self.future = SenderFuture::Submitted(tokio::spawn(async { future.await }));
+                true
+            }
+            future => {
+                self.future = future;
+                false
+            }
+        };
+
+        // Skip the [] of the Vec
+        let data = &data[..data.len() - 1];
+        let mut data = data.to_vec();
+        if !first {
+            data[0] = b',';
+        }
+        self.sender.send_data(Bytes::from(data)).await?;
+
+        self.payloads += 1;
+        Ok(())
+    }
+
+    pub async fn finish(mut self) -> anyhow::Result<u32> {
+        if let SenderFuture::Submitted(future) = self.future {
+            // insert a trailing ]
+            if self.needs_boundary {
+                self.sender.send_data(concat!("]\r\n", BOUNDARY_LINE).into()).await?;
+            } else {
+                self.sender.send_data(Bytes::from_static(b"]")).await?;
+            }
+
+            drop(self.sender);
+            match future.await? {
+                Ok(response) => {
+                    let status = response.status().as_u16();
+                    if status >= 400 {
+                        let body_bytes = hyper::body::to_bytes(response.into_body()).await?;
+                        let response_body = String::from_utf8(body_bytes.to_vec()).unwrap_or_default();
+                        anyhow::bail!("Server did not accept debugger payload ({status}): {response_body}");
+                    }
+                    Ok(self.payloads)
+                }
+                Err(e) => anyhow::bail!("Failed to send traces: {e}"),
+            }
+        } else {
+            Ok(0)
+        }
+    }
+}
+
 pub async fn send(
     payload: &[u8],
     config: &Config,
     debugger_type: DebuggerType,
     percent_encoded_tags: &str,
 ) -> anyhow::Result<()> {
-    // SAFETY: we ensure the reference exists across the request
-    let payload = unsafe { std::mem::transmute::<&[u8], &[u8]>(payload) };
-
-    let endpoint = match debugger_type {
-        DebuggerType::Diagnostics => &config.diagnostics_endpoint,
-        DebuggerType::Logs => &config.logs_endpoint,
-    }.as_ref().unwrap();
-
-    let mut url = endpoint.url.clone();
-    let mut parts = url.into_parts();
-    let query = format!(
-        "{}?ddtags={}",
-        parts.path_and_query.unwrap(),
-        percent_encoded_tags
-    );
-    parts.path_and_query = Some(PathAndQuery::from_str(&query)?);
-    url = Uri::from_parts(parts)?;
-
-    let mut req = hyper::Request::builder()
-        .header(
-            hyper::header::USER_AGENT,
-            concat!("Tracer/", env!("CARGO_PKG_VERSION")),
-        )
-        .method(Method::POST)
-        .uri(url);
-
-    if endpoint.api_key.is_some() {
-        req = req.header("DD-EVP-ORIGIN", "agent-debugger");
-    }
-
-    let req = if debugger_type == DebuggerType::Diagnostics {
-        const BOUNDARY: &str = "------------------------44617461646f67";
-        fn event_json(payload: &[u8]) -> Vec<u8> {
-            fn write_boundary(data: &mut Vec<u8>) {
-                data.extend_from_slice(b"--");
-                data.extend_from_slice(BOUNDARY.as_bytes());
-                data.extend_from_slice(b"\r\n");
-            }
-            let mut data = Vec::new();
-
-            write_boundary(&mut data);
-            data.extend_from_slice(b"Content-Disposition: form-data; name=\"event\"; filename=\"event.json\"\r\n");
-            data.extend_from_slice(b"Content-Type: application/json\r\n");
-            data.extend_from_slice(b"\r\n");
-            data.extend_from_slice(payload);
-            data.extend_from_slice(b"\r\n");
-            write_boundary(&mut data);
-
-            data
-        }
-        req.header("Content-type", format!("multipart/form-data; boundary={}", BOUNDARY))
-           .body(Body::from(event_json(payload)))
-    } else {
-        req.header("Content-type", "application/json")
-           .body(Body::from(payload))
-    }?;
-
-    match Client::builder()
-        .build(Connector::default())
-        .request(req)
-        .await
-    {
-        Ok(response) => {
-            let status = response.status().as_u16();
-            if status >= 400 {
-                let body_bytes = hyper::body::to_bytes(response.into_body()).await?;
-                let response_body = String::from_utf8(body_bytes.to_vec()).unwrap_or_default();
-                anyhow::bail!("Server did not accept debugger payload ({status}): {response_body}");
-            }
-            Ok(())
-        }
-        Err(e) => anyhow::bail!("Failed to send traces: {e}"),
-    }
+    let mut batch = PayloadSender::new(config, debugger_type, percent_encoded_tags)?;
+    batch.append(payload).await?;
+    batch.finish().await?;
+    Ok(())
 }
 
 pub fn generate_new_id() -> Uuid {
