9a61b260ca773fd6b6feeac32ccccca8977a7bca
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-07-17 22:47:15 -0400

    compiles

--- profiling-ffi/src/array_queue.rs
+++ profiling-ffi/src/array_queue.rs
@@ -1,59 +1,70 @@
-use crossbeam_queue::ArrayQueue as CrossbeamArrayQueue;
-use std::os::raw::c_void;
+use std::ffi::c_void;
+use std::ptr::NonNull;
 
-#[repr(C)]
+#[allow(dead_code)]
 pub struct ArrayQueue {
-    inner: *mut CrossbeamArrayQueue<*mut c_void>,
+    inner: Box<crossbeam_queue::ArrayQueue<*mut c_void>>,
 }
 
 #[no_mangle]
-pub extern "C" fn queue_new(capacity: usize) -> *mut ArrayQueue {
-    let queue = Box::new(CrossbeamArrayQueue::new(capacity));
-    let wrapper = Box::new(ArrayQueue {
-        inner: Box::into_raw(queue),
+pub extern "C" fn array_queue_new(out_handle: NonNull<Box<ArrayQueue>>, capacity: usize) -> bool {
+    let queue = Box::new(ArrayQueue {
+        inner: Box::new(crossbeam_queue::ArrayQueue::new(capacity)),
     });
-    Box::into_raw(wrapper)
-}
-
-#[no_mangle]
-pub extern "C" fn queue_drop(queue: *mut ArrayQueue) {
-    if !queue.is_null() {
-        unsafe { drop(Box::from_raw(queue)) };
+    unsafe {
+        out_handle.as_ptr().write(queue);
     }
+    true
 }
 
-// crossbeam_queue::ArrayQueue also implements force_push, which
-#[no_mangle]
-pub extern "C" fn queue_push(queue: *mut ArrayQueue, value: *mut c_void) -> bool {
-    let queue = unsafe { &(*queue) };
-    let inner = unsafe { &*queue.inner };
-    inner.push(value).is_ok()
-}
+// #[no_mangle]
+// pub extern "C" fn queue_new(capacity: usize) -> *mut ArrayQueueWrapper {
+//     let queue = Box::new(ArrayQueue::new(capacity));
+//     let wrapper = Box::new(ArrayQueueWrapper {
+//         inner: Box::into_raw(queue),
+//     });
+//     Box::into_raw(wrapper)
+// }
 
-#[no_mangle]
-pub extern "C" fn queue_pop(queue: *mut ArrayQueue) -> *mut c_void {
-    let queue = unsafe { &(*queue) };
-    let inner = unsafe { &*queue.inner };
-    inner.pop().unwrap_or(std::ptr::null_mut())
-}
+// #[no_mangle]
+// pub extern "C" fn queue_drop(queue: *mut ArrayQueueWrapper) {
+//     if !queue.is_null() {
+//         unsafe { drop(Box::from_raw(queue)) };
+//     }
+// }
 
-#[no_mangle]
-pub extern "C" fn queue_capacity(queue: *mut ArrayQueue) -> usize {
-    let queue = unsafe { &(*queue) };
-    let inner = unsafe { &*queue.inner };
-    inner.capacity()
-}
+// // crossbeam_queue::ArrayQueue also implements force_push, which
+// #[no_mangle]
+// pub extern "C" fn queue_push(queue: *mut ArrayQueueWrapper, value: *mut c_void) -> bool {
+//     let queue = unsafe { &(*queue) };
+//     let inner = unsafe { &*queue.inner };
+//     inner.push(value).is_ok()
+// }
 
-#[no_mangle]
-pub extern "C" fn queue_len(queue: *mut ArrayQueue) -> usize {
-    let queue = unsafe { &(*queue) };
-    let inner = unsafe { &*queue.inner };
-    inner.len()
-}
+// #[no_mangle]
+// pub extern "C" fn queue_pop(queue: *mut ArrayQueueWrapper) -> *mut c_void {
+//     let queue = unsafe { &(*queue) };
+//     let inner = unsafe { &*queue.inner };
+//     inner.pop().unwrap_or(std::ptr::null_mut())
+// }
 
-#[no_mangle]
-pub extern "C" fn queue_is_empty(queue: *mut ArrayQueue) -> bool {
-    let queue = unsafe { &(*queue) };
-    let inner = unsafe { &*queue.inner };
-    inner.is_empty()
-}
+// #[no_mangle]
+// pub extern "C" fn queue_capacity(queue: *mut ArrayQueueWrapper) -> usize {
+//     let queue = unsafe { &(*queue) };
+//     let inner = unsafe { &*queue.inner };
+//     inner.capacity()
+// }
+
+// #[no_mangle]
+// pub extern "C" fn queue_len(queue: *mut ArrayQueueWrapper) -> usize {
+//     let queue = unsafe { &(*queue) };
+//     let inner = unsafe { &*queue.inner };
+//     inner.len()
+// }
+
+// #[no_mangle]
+// pub extern "C" fn queue_is_empty(queue: *mut ArrayQueueWrapper) -> bool {
+//     let queue = unsafe { &(*queue) };
+//     let inner = unsafe { &*queue.inner };
+//     inner.is_empty()
+// }
