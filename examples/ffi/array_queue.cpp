0238f23ac67a9249f6d42cddf35830f2357be06e
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-07-24 16:55:16 +0000

    delete when error

--- examples/ffi/array_queue.cpp
+++ examples/ffi/array_queue.cpp
@@ -77,6 +77,7 @@ int main(void) {
         } else {
           print_error("Failed to push to array queue", push_result.err);
           ddog_Error_drop(&push_result.err);
+          delete sample;
           return;
         }
       }
