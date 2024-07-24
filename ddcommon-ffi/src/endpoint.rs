fc2aed23b6d621b15ff4facfa748f3f8fee923d6
Daniel Schwartz-Narbonne <daniel.schwartznarbonne@datadoghq.com>
2024-07-24 15:15:09 -0400

    ddcommon endpoint explicit

--- ddcommon-ffi/src/endpoint.rs
+++ ddcommon-ffi/src/endpoint.rs
@@ -3,16 +3,16 @@
 
 use crate::slice::AsBytes;
 use crate::Error;
-use ddcommon::{parse_uri, Endpoint};
+use ddcommon::{parse_uri};
 use hyper::http::uri::{Authority, Parts};
 use std::str::FromStr;
 
 #[no_mangle]
 #[must_use]
-pub extern "C" fn ddog_endpoint_from_url(url: crate::CharSlice) -> Option<Box<Endpoint>> {
+pub extern "C" fn ddog_endpoint_from_url(url: crate::CharSlice) -> Option<Box<ddcommon::Endpoint>> {
     parse_uri(url.to_utf8_lossy().as_ref())
         .ok()
-        .map(|url| Box::new(Endpoint::from_url(url)))
+        .map(|url| Box::new(ddcommon::Endpoint::from_url(url)))
 }
 
 #[no_mangle]
@@ -26,10 +26,10 @@ pub extern "C" fn ddog_endpoint_from_filename(filename: crate::CharSlice) -> Opt
 // own subdomains.
 #[no_mangle]
 #[must_use]
-pub extern "C" fn ddog_endpoint_from_api_key(api_key: crate::CharSlice) -> Box<Endpoint> {
+pub extern "C" fn ddog_endpoint_from_api_key(api_key: crate::CharSlice) -> Box<ddcommon::Endpoint> {
     let mut parts = Parts::default();
     parts.authority = Some(Authority::from_static("datadoghq.com"));
-    Box::new(Endpoint {
+    Box::new(ddcommon::Endpoint {
         url: hyper::Uri::from_parts(parts).unwrap(),
         api_key: Some(api_key.to_utf8_lossy().to_string().into()),
         ..Default::default()
@@ -43,14 +43,14 @@ pub extern "C" fn ddog_endpoint_from_api_key(api_key: crate::CharSlice) -> Box<E
 pub extern "C" fn ddog_endpoint_from_api_key_and_site(
     api_key: crate::CharSlice,
     site: crate::CharSlice,
-    endpoint: &mut *mut Endpoint,
+    endpoint: &mut *mut ddcommon::Endpoint,
 ) -> Option<Box<Error>> {
     let mut parts = Parts::default();
     parts.authority = Some(match Authority::from_str(&site.to_utf8_lossy()) {
         Ok(s) => s,
         Err(e) => return Some(Box::new(Error::from(e.to_string()))),
     });
-    *endpoint = Box::into_raw(Box::new(Endpoint {
+    *endpoint = Box::into_raw(Box::new(ddcommon::Endpoint {
         url: hyper::Uri::from_parts(parts).unwrap(),
         api_key: Some(api_key.to_utf8_lossy().to_string().into()),
         ..Default::default()
@@ -59,12 +59,12 @@ pub extern "C" fn ddog_endpoint_from_api_key_and_site(
 }
 
 #[no_mangle]
-extern "C" fn ddog_endpoint_set_timeout(endpoint: &mut Endpoint, millis: u64) {
+extern "C" fn ddog_endpoint_set_timeout(endpoint: &mut ddcommon::Endpoint, millis: u64) {
     endpoint.timeout_ms = millis;
 }
 
 #[no_mangle]
-pub extern "C" fn ddog_endpoint_drop(_: Box<Endpoint>) {}
+pub extern "C" fn ddog_endpoint_drop(_: Box<ddcommon::Endpoint>) {}
 
 #[cfg(test)]
 mod tests {
@@ -96,14 +96,14 @@ mod tests {
         let mut endpoint = ddog_endpoint_from_url(url);
         assert_eq!(
             endpoint.as_ref().unwrap().timeout_ms,
-            Endpoint::DEFAULT_TIMEOUT
+            ddcommon::Endpoint::DEFAULT_TIMEOUT
         );
 
         ddog_endpoint_set_timeout(endpoint.as_mut().unwrap(), 2000);
         assert_eq!(endpoint.unwrap().timeout_ms, 2000);
 
         let mut endpoint_api_key = ddog_endpoint_from_api_key(CharSlice::from("test-key"));
-        assert_eq!(endpoint_api_key.timeout_ms, Endpoint::DEFAULT_TIMEOUT);
+        assert_eq!(endpoint_api_key.timeout_ms, ddcommon::Endpoint::DEFAULT_TIMEOUT);
 
         ddog_endpoint_set_timeout(&mut endpoint_api_key, 2000);
         assert_eq!(endpoint_api_key.timeout_ms, 2000);
