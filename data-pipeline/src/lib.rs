66975a4e480d08e93a6d06e955054d1b33d8dc4a
Andrew Glaude <andrew.glaude@datadoghq.com>
2024-09-09 14:54:28 -0400

    rust fmt

--- data-pipeline/src/lib.rs
+++ data-pipeline/src/lib.rs
@@ -2,8 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 #![deny(missing_docs)]
 
-//! TraceExporter provides a minimum viable product (MVP) to send traces to agents. The aim of the project at this
-//! state is to provide a basic API in order to test its viability and integration in different languages.
+//! TraceExporter provides a minimum viable product (MVP) to send traces to agents. The aim of the
+//! project at this state is to provide a basic API in order to test its viability and integration
+//! in different languages.
 
 #[allow(missing_docs)]
 pub mod span_concentrator;
