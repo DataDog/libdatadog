62692ccad53b68d05e4425001cbde27a3a4211a0
Björn Antonsson <bjorn.antonsson@datadoghq.com>
2024-09-13 08:34:29 +0200

    Move benchmarks to separate project

--- benchmark/run_benchmarks_ci.sh
+++ benchmark/run_benchmarks_ci.sh
@@ -21,9 +21,30 @@ OUTPUT_DIR="${1:-}"
 pushd "${PROJECT_DIR}" > /dev/null
 
 # Run benchmarks
-message "Running benchmarks"
+message "Running benchmarks in project"
 cargo bench --workspace -- --warm-up-time 1 --measurement-time 5 --sample-size=200
-message "Finished running benchmarks"
+message "Finished running benchmarks in project"
+
+# Copy the benchmark results to the output directory
+if [[ -n "${OUTPUT_DIR}" && -d "target" ]]; then
+  # Is this a relative path?
+  if [[ "$OUTPUT_DIR" != /* ]]; then
+    OUTPUT_DIR="${CURRENT_PATH}/${OUTPUT_DIR}"
+  fi
+  message "Copying benchmark results to ${OUTPUT_DIR}"
+  pushd target > /dev/null
+  find criterion -type d -regex '.*/new$' | while read -r dir; do
+    mkdir -p "${OUTPUT_DIR}/${dir}"
+    cp -r "${dir}"/* "${OUTPUT_DIR}/${dir}"
+  done
+  popd > /dev/null
+fi
+
+pushd "${SCRIPT_DIR}" > /dev/null
+# Run benchmarks
+message "Running benchmarks in benchmark"
+cargo bench --workspace -- --warm-up-time 1 --measurement-time 5 --sample-size=200
+message "Finished running benchmarks in benchmark"
 
 # Copy the benchmark results to the output directory
 if [[ -n "${OUTPUT_DIR}" && -d "target" ]]; then
