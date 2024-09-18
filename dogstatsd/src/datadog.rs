d873333c0667c9e4d29003d1e4f66b3b4110b345
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2024-09-18 13:22:27 -0400

    fix: missing error dep

--- dogstatsd/src/datadog.rs
+++ dogstatsd/src/datadog.rs
@@ -8,7 +8,7 @@ use protobuf::Message;
 use reqwest;
 use serde::{Serialize, Serializer};
 use serde_json;
-use tracing::debug;
+use tracing::{debug, error};
 
 /// Interface for the `DogStatsD` metrics intake API.
 #[derive(Debug)]
