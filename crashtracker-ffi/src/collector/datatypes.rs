a119678b487418955a0f07fdaa8f9547f7bfa908
sanchda <838104+sanchda@users.noreply.github.com>
2024-10-30 14:39:05 +0000

    Fix FFI issue

--- crashtracker-ffi/src/collector/datatypes.rs
+++ crashtracker-ffi/src/collector/datatypes.rs
@@ -83,16 +83,14 @@ impl<'a> TryFrom<Config<'a>> for datadog_crashtracker::CrashtrackerConfiguration
             vec
         };
         // Verify that the timeout is within the bounds of an i32
-        let timeout_ms = {
-            if value.timeout_ms > i32::MAX as u32 {
-                return Err(anyhow::anyhow!(
-                    "Timeout value {} is too large to fit in an i32",
-                    value.timeout_ms
-                ));
-            } else if value.timeout_ms == 0 {
-                // This is a common way of specifying a default, so choose 10s.
-                return Ok(10_000);
-            }
+        let timeout_ms = if value.timeout_ms > i32::MAX as u32 {
+            return Err(anyhow::anyhow!(
+                "Timeout value {} is too large to fit in an i32",
+                value.timeout_ms
+            ));
+        } else if value.timeout_ms == 0 {
+            10_000 // We hardcode a magical default of 10s here
+        } else {
             value.timeout_ms
         };
         let create_alt_stack = value.create_alt_stack;
