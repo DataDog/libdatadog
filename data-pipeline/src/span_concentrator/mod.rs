d66b002014021f7922456300c00381bfeddb6f24
vianney <vianney.ruhlmann@datadoghq.com>
2024-12-06 16:07:30 +0100

    [APMSP-1331] Reduce allocations in client-side stats (#774)
    * Use Cow in AggregationKey fields
    
    * Add docs
    APMSP-1331

--- data-pipeline/src/span_concentrator/mod.rs
+++ data-pipeline/src/span_concentrator/mod.rs
@@ -124,7 +124,7 @@ impl SpanConcentrator {
                 bucket_timestamp = self.oldest_timestamp;
             }
 
-            let agg_key = AggregationKey::from_span(span, &self.peer_tag_keys);
+            let agg_key = AggregationKey::from_span(span, self.peer_tag_keys.as_slice());
 
             self.buckets
                 .entry(bucket_timestamp)
@@ -136,7 +136,7 @@ impl SpanConcentrator {
     /// Flush all stats bucket except for the `buffer_len` most recent. If `force` is true, flush
     /// all buckets.
     pub fn flush(&mut self, now: SystemTime, force: bool) -> Vec<pb::ClientStatsBucket> {
-        // TODO: Use drain filter from hashbrown to avoid removing current buckets
+        // TODO: Wait for HashMap::extract_if to be stabilized to avoid a full drain
         let now_timestamp = system_time_to_unix_duration(now).as_nanos() as u64;
         let buckets: Vec<(u64, StatsBucket)> = self.buckets.drain().collect();
         self.oldest_timestamp = if force {
