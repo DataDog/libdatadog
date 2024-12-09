715028a3314edc312e67caf752cfe85fad7ea4d2
nina9753 <nina.rei@datadoghq.com>
2024-12-09 14:22:57 -0500

    add create_test_gcp_span

--- trace-utils/src/test_utils/mod.rs
+++ trace-utils/src/test_utils/mod.rs
@@ -118,6 +118,52 @@ pub fn create_test_span(
     span
 }
 
+pub fn create_test_gcp_span(
+    trace_id: u64,
+    span_id: u64,
+    parent_id: u64,
+    start: i64,
+    is_top_level: bool,
+) -> pb::Span {
+    let mut span = pb::Span {
+        trace_id,
+        span_id,
+        service: "test-service".to_string(),
+        name: "test_name".to_string(),
+        resource: "test-resource".to_string(),
+        parent_id,
+        start,
+        duration: 5,
+        error: 0,
+        meta: HashMap::from([
+            ("service".to_string(), "test-service".to_string()),
+            ("env".to_string(), "test-env".to_string()),
+            ("runtime-id".to_string(), "test-runtime-id-value".to_string()),
+        ]),
+        metrics: HashMap::new(),
+        r#type: "".to_string(),
+        meta_struct: HashMap::new(),
+        span_links: vec![],
+    };
+    if is_top_level {
+        span.metrics.insert("_top_level".to_string(), 1.0);
+        span.meta
+            .insert("_dd.origin".to_string(), "cloudfunction".to_string());
+        span.meta
+            .insert("origin".to_string(), "cloudfunction".to_string());
+        span.meta.insert(
+            "functionname".to_string(),
+            "dummy_function_name".to_string(),
+        );
+        span.meta.insert("gcrfx.project_id".to_string(), "dummy_project_id".to_string());
+        span.meta.insert("gcrfx.location".to_string(), "dummy_region_west".to_string());
+        span.meta.insert("version".to_string(), "dummy_version".to_string());
+        span.meta.insert("_dd.gcrfx.resource_name".to_string(), "projects/dummy_project_id/locations/dummy_region_west/functions/dummy_function_name".to_string());
+        span.r#type = "serverless".to_string();
+    }
+    span
+}
+
 pub fn create_test_json_span(
     trace_id: u64,
     span_id: u64,
