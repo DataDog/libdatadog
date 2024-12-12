354bef5fb042af0e000bef227385690b53a75ec7
nina9753 <nina.rei@datadoghq.com>
2024-12-12 14:01:08 -0500

    lint

--- trace-utils/src/trace_utils.rs
+++ trace-utils/src/trace_utils.rs
@@ -520,10 +520,14 @@ pub fn enrich_span_with_mini_agent_metadata(
 pub fn enrich_span_with_google_cloud_function_metadata(
     span: &mut pb::Span,
     mini_agent_metadata: &MiniAgentMetadata,
-    function: Option<String>
+    function: Option<String>,
 ) {
-    let Some(region) = &mini_agent_metadata.gcp_region else { return; };
-    let Some(project) = &mini_agent_metadata.gcp_project_id else { return; };
+    let Some(region) = &mini_agent_metadata.gcp_region else {
+        return;
+    };
+    let Some(project) = &mini_agent_metadata.gcp_project_id else {
+        return;
+    };
     if function.is_some() && !region.is_empty() && !project.is_empty() {
         let resource_name = format!(
             "projects/{}/locations/{}/functions/{}",
