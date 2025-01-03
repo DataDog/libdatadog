fdfaa1f675bf240abeb8a4f431a3f31a3326624a
Ivo Anjo <ivo.anjo@datadoghq.com>
2025-01-03 11:45:01 +0000

    Apply another clippy suggestion

--- profiling-ffi/src/profiles.rs
+++ profiling-ffi/src/profiles.rs
@@ -546,7 +546,7 @@ pub unsafe extern "C" fn ddog_prof_Profile_add(
 ) -> ProfileResult {
     (|| {
         let profile = profile_ptr_to_inner(profile)?;
-        let uses_string_ids = sample.labels.first().map_or(false, |label| {
+        let uses_string_ids = sample.labels.first().is_some_and(|label| {
             label.key.is_empty() && label.key_id.value > 0
         });
 
