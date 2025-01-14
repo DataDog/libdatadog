933368f686152aba9a86310489ac504b1492332e
paullegranddc <paul.legranddescloizeaux@datadoghq.com>
2025-01-14 20:40:14 +0100

    Add license header

--- library-config/src/lib.rs
+++ library-config/src/lib.rs
@@ -1,3 +1,6 @@
+// Copyright 2021-Present Datadog, Inc. https://www.datadoghq.com/
+// SPDX-License-Identifier: Apache-2.0
+
 use std::borrow::Cow;
 use std::cell::OnceCell;
 use std::collections::HashMap;
