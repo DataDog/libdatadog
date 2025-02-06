c7c588c46cfa9c00384926a1f7d9b6e3913fa762
Dylan Yang <dylan.yang@datadoghq.com>
2025-02-06 13:39:43 -0500

    other serverless types

--- dogstatsd/src/metrics_origins.rs
+++ dogstatsd/src/metrics_origins.rs
@@ -1,47 +1,17 @@
 use datadog_protos::metrics::{Metadata, Origin};
 use protobuf::MessageField;
 
+const AZURE_APP_SERVICES_PREFIX: &str = "azure.app_services";
+const GOOGLE_CLOUD_RUN_PREFIX: &str = "gcp.run";
+const AZURE_CONTAINER_APP_PREFIX: &str = "azure.app_containerapps";
 const AWS_LAMBDA_PREFIX: &str = "aws.lambda";
 const AWS_STEP_FUNCTIONS_PREFIX: &str = "aws.states";
 
+/// Represents the product origin of a metric.
+/// The full enum is exhaustive so we only include what we need. Please reference the corresponding enum for all possible values 
+/// https://github.com/DataDog/dd-source/blob/573dee9b5f7ee13935cb3ad11b16dde970528983/domains/metrics/shared/libs/proto/origin/origin.proto#L161
 pub enum OriginProduct {
-    None,
-    Serverless,
-    APM,
-    Logs,
-    Processes,
-    RUM,
-    Events,
-    Synthetics,
-    MetricsAPI,
-    USM,
-    Agent,
-    CSM,
-    CloudIntegrations,
-    APICatalog,
-    Vector,
-    ObservabilityPipelines,
-    DSM,
-    DatadogPlatform,
-    SAASIntegrations,
-    DatadogExporter,
-    NPM,
-    DBM,
-    ServiceCatalog,
-    LLMObservability,
-    SyncCLI,
-    AppBuilder,
-    Profiling,
-    WorkflowAutomation,
-    DQM,
-    SoftwareDelivery,
-    CloudCostManagement,
-    DJM,
-    Containers,
-    ServiceCheck,
-    DatadogOperator,
-    SLO,
-    StorageMonitoring,
+    Serverless = 1,
 }
 
 impl Into<u32> for OriginProduct {
@@ -50,81 +20,31 @@ impl Into<u32> for OriginProduct {
     }
 }
 
+/// Represents the category origin of a metric.
+/// The full enum is exhaustive so we only include what we need. Please reference the corresponding enum for all possible values 
+/// https://github.com/DataDog/dd-source/blob/573dee9b5f7ee13935cb3ad11b16dde970528983/domains/metrics/shared/libs/proto/origin/origin.proto#L276
 pub enum OriginCategory {
-    Other,
-    Reserved1,
-    Traces,
-    Spans,
-    LogMetrics,
-    ProcessMetrics,
-    RumMetrics,
-    EventMetrics,
-    SyntheticsMetrics,
-    DistributionMetrics,
-    Dogstatsd,
-    Integration,
-    UsageMetrics,
-    Reserved13,
-    ApmTraceInternal,
-    USMMetrics,
-    DSMMetrics,
-    OTLP,
-    AWS,
-    GoogleCloud,
-    Azure,
-    SAAS,
-    DatabaseQueryMetrics,
-    DatabaseProcedureMetrics,
-    DatabaseLockMetrics,
-    DatabaseActivityMetrics,
-    ScorecardMetrics,
-    LLMObservabilityMetrics,
-    Datadogpy,
-    DatadogAPIClientPython,
-    DatadogAPIClientGo,
-    DatadogAPIClientRust,
-    DatadogAPIClientJava,
-    DatadogAPIClientRuby,
-    DatadogAPIClientTypeScript,
-    AppServicesMetrics,
-    CloudRunMetrics,
-    ContainerAppMetrics,
-    LambdaMetrics,
-    OTLPIntegration,
-    OTLPOther,
-    StepFunctionsMetrics,
-    DatabaseAgentJobsMetrics,
-    RateLimiting,
-    Accupath,
-    ServiceIndex,
-    ServiceMap,
-    SpanToMetrics,
-    TraceMetrics,
-    TracerAnalyticsMetrics,
-    TracerRuntimeMetrics,
-    AppBuilderOOTBDashboard,
-    WorkflowAutomationExecutionMetrics,
-    DQMMetrics,
-    AgentCI,
-    CodeCoverage,
-    MetricsTagManagement,
-    IntegrationHealth,
-    PrivateActionsRunner,
-    ContainerImages,
-    OracleCloudInfrastructure,
-    WorkflowAutomationCreationMetrics,
-    AmazonS3,
-    AzureBlobStorage,
-    DatadogAgent,
-    DatadogAgentProfile,
-    DatadogDashboard,
-    DatadogMonitor,
-    DatadogSLO,
-    GoogleCloudStorage,
-    OracleCloudInfrastructureObjectStorage,
+    AppServicesMetrics = 35,
+    CloudRunMetrics = 36,
+    ContainerAppMetrics = 37,
+    LambdaMetrics = 38,
+    StepFunctionsMetrics = 41,
 }
 
 impl Into<u32> for OriginCategory {
+    fn into(self) -> u32 {
+        self as u32
+    }
+}
+
+/// Represents the service origin of a metric.
+/// The full enum is exhaustive so we only include what we need. Please reference the corresponding enum for all possible values 
+/// https://github.com/DataDog/dd-source/blob/573dee9b5f7ee13935cb3ad11b16dde970528983/domains/metrics/shared/libs/proto/origin/origin.proto#L417
+pub enum OriginService {
+    Other = 0,
+}
+
+impl Into<u32> for OriginService {
     fn into(self) -> u32 {
         self as u32
     }
@@ -134,11 +54,38 @@ pub fn get_metric_origin(name: &str) -> Option<Metadata> {
     let prefix = name.split('.').take(2).collect::<Vec<&str>>().join(".");
 
     match prefix {
+        _ if prefix == AZURE_APP_SERVICES_PREFIX => Some(Metadata {
+            origin: MessageField::some(Origin {
+                origin_product: OriginProduct::Serverless.into(),
+                origin_category: OriginCategory::AppServicesMetrics.into(),
+                origin_service: OriginService::Other.into(),
+                special_fields: Default::default(),
+            }),
+            ..Default::default()
+        }),
+        _ if prefix == GOOGLE_CLOUD_RUN_PREFIX => Some(Metadata {
+            origin: MessageField::some(Origin {
+                origin_product: OriginProduct::Serverless.into(),
+                origin_category: OriginCategory::CloudRunMetrics.into(),
+                origin_service: OriginService::Other.into(),
+                special_fields: Default::default(),
+            }),
+            ..Default::default()
+        }),
+        _ if prefix == AZURE_CONTAINER_APP_PREFIX => Some(Metadata {
+            origin: MessageField::some(Origin {
+                origin_product: OriginProduct::Serverless.into(),
+                origin_category: OriginCategory::ContainerAppMetrics.into(),
+                origin_service: OriginService::Other.into(),
+                special_fields: Default::default(),
+            }),
+            ..Default::default()
+        }),
         _ if prefix == AWS_LAMBDA_PREFIX => Some(Metadata {
             origin: MessageField::some(Origin {
                 origin_product: OriginProduct::Serverless.into(),
                 origin_category: OriginCategory::LambdaMetrics.into(),
-                origin_service: 0, // uncategorized
+                origin_service: OriginService::Other.into(),
                 special_fields: Default::default(),
             }),
             ..Default::default()
@@ -147,7 +94,7 @@ pub fn get_metric_origin(name: &str) -> Option<Metadata> {
             origin: MessageField::some(Origin {
                 origin_product: OriginProduct::Serverless.into(),
                 origin_category: OriginCategory::StepFunctionsMetrics.into(),
-                origin_service: 0, // uncategorized
+                origin_service: OriginService::Other.into(),
                 special_fields: Default::default(),
             }),
             ..Default::default()
