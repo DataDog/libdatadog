5bc794c0b5d6c2f6b3b96fe7e23e3dbeb16cb8e4
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2025-02-19 13:27:04 -0500

    format

--- dogstatsd/src/datadog.rs
+++ dogstatsd/src/datadog.rs
@@ -151,7 +151,8 @@ impl DdApi {
     /// Ship a serialized series to the API, blocking
     pub async fn ship_series(&self, series: &Series) -> Result<Response, ShippingError> {
         let url = format!("{}/api/v2/series", &self.metrics_intake_url_prefix);
-        let safe_body = serde_json::to_vec(&series).map_err(|_| ShippingError::Payload("Failed to serialize series".to_string()))?;
+        let safe_body = serde_json::to_vec(&series)
+            .map_err(|_| ShippingError::Payload("Failed to serialize series".to_string()))?;
         debug!("Sending body: {:?}", &series);
         self.ship_data(url, safe_body, "application/json").await
     }
