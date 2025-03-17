cde6b435f59922c42848cb2643d25cf8807fc101
paullegranddc <82819397+paullegranddc@users.noreply.github.com>
2025-03-17 13:30:12 +0100

    refactor(trace-utils): make flate2 and and hyper-proxy dependencies optionnal  (#929)
    # What does this PR do?
    
    * Add a new feature "mini_agent" on trace_utils and do not use it bydefault. This is going to be used to progressively separate code used by only the mini agent from common code=
    * Hide flate2 usage under the compression flag
    * Hide hyper-proxy dependency under the proxy feature flag

--- trace-utils/src/stats_utils.rs
+++ trace-utils/src/stats_utils.rs
@@ -1,82 +1,89 @@
 // Copyright 2023-Present Datadog, Inc. https://www.datadoghq.com/
 // SPDX-License-Identifier: Apache-2.0
 
-use flate2::{write::GzEncoder, Compression};
-use hyper::body::HttpBody;
-use hyper::{body::Buf, Body, Client, Method, Request, StatusCode};
-use log::debug;
-use std::io::Write;
+#[cfg(feature = "mini_agent")]
+pub use mini_agent::*;
 
-use datadog_trace_protobuf::pb;
-use ddcommon::connector::Connector;
-use ddcommon::Endpoint;
+#[cfg(feature = "mini_agent")]
+mod mini_agent {
+    use hyper::body::HttpBody;
+    use hyper::{body::Buf, Body, Client, Method, Request, StatusCode};
+    use log::debug;
+    use std::io::Write;
 
-pub async fn get_stats_from_request_body(body: Body) -> anyhow::Result<pb::ClientStatsPayload> {
-    let buffer = body.collect().await?.aggregate();
+    use datadog_trace_protobuf::pb;
+    use ddcommon::connector::Connector;
+    use ddcommon::Endpoint;
 
-    let client_stats_payload: pb::ClientStatsPayload = match rmp_serde::from_read(buffer.reader()) {
-        Ok(res) => res,
-        Err(err) => {
-            anyhow::bail!("Error deserializing stats from request body: {err}")
+    pub async fn get_stats_from_request_body(body: Body) -> anyhow::Result<pb::ClientStatsPayload> {
+        let buffer = body.collect().await?.aggregate();
+
+        let client_stats_payload: pb::ClientStatsPayload =
+            match rmp_serde::from_read(buffer.reader()) {
+                Ok(res) => res,
+                Err(err) => {
+                    anyhow::bail!("Error deserializing stats from request body: {err}")
+                }
+            };
+
+        if client_stats_payload.stats.is_empty() {
+            debug!("Empty trace stats payload received, but this is okay");
         }
-    };
-
-    if client_stats_payload.stats.is_empty() {
-        debug!("Empty trace stats payload received, but this is okay");
+        Ok(client_stats_payload)
     }
-    Ok(client_stats_payload)
-}
 
-pub fn construct_stats_payload(stats: Vec<pb::ClientStatsPayload>) -> pb::StatsPayload {
-    pb::StatsPayload {
-        agent_hostname: "".to_string(),
-        agent_env: "".to_string(),
-        stats,
-        agent_version: "".to_string(),
-        client_computed: true,
-        split_payload: false,
+    pub fn construct_stats_payload(stats: Vec<pb::ClientStatsPayload>) -> pb::StatsPayload {
+        pb::StatsPayload {
+            agent_hostname: "".to_string(),
+            agent_env: "".to_string(),
+            stats,
+            agent_version: "".to_string(),
+            client_computed: true,
+            split_payload: false,
+        }
     }
-}
 
-pub fn serialize_stats_payload(payload: pb::StatsPayload) -> anyhow::Result<Vec<u8>> {
-    let msgpack = rmp_serde::to_vec_named(&payload)?;
-    let mut encoder = GzEncoder::new(Vec::new(), Compression::fast());
-    encoder.write_all(&msgpack)?;
-    match encoder.finish() {
-        Ok(res) => Ok(res),
-        Err(e) => anyhow::bail!("Error serializing stats payload: {e}"),
+    pub fn serialize_stats_payload(payload: pb::StatsPayload) -> anyhow::Result<Vec<u8>> {
+        let msgpack = rmp_serde::to_vec_named(&payload)?;
+        let mut encoder = flate2::write::GzEncoder::new(Vec::new(), flate2::Compression::fast());
+        encoder.write_all(&msgpack)?;
+        match encoder.finish() {
+            Ok(res) => Ok(res),
+            Err(e) => anyhow::bail!("Error serializing stats payload: {e}"),
+        }
     }
-}
 
-pub async fn send_stats_payload(
-    data: Vec<u8>,
-    target: &Endpoint,
-    api_key: &str,
-) -> anyhow::Result<()> {
-    let req = Request::builder()
-        .method(Method::POST)
-        .uri(target.url.clone())
-        .header("Content-Type", "application/msgpack")
-        .header("Content-Encoding", "gzip")
-        .header("DD-API-KEY", api_key)
-        .body(Body::from(data.clone()))?;
+    pub async fn send_stats_payload(
+        data: Vec<u8>,
+        target: &Endpoint,
+        api_key: &str,
+    ) -> anyhow::Result<()> {
+        let req = Request::builder()
+            .method(Method::POST)
+            .uri(target.url.clone())
+            .header("Content-Type", "application/msgpack")
+            .header("Content-Encoding", "gzip")
+            .header("DD-API-KEY", api_key)
+            .body(Body::from(data.clone()))?;
 
-    let client: Client<_, hyper::Body> = Client::builder().build(Connector::default());
-    match client.request(req).await {
-        Ok(response) => {
-            if response.status() != StatusCode::ACCEPTED {
-                let body_bytes = response.into_body().collect().await?.to_bytes();
-                let response_body = String::from_utf8(body_bytes.to_vec()).unwrap_or_default();
-                anyhow::bail!("Server did not accept trace stats: {response_body}");
+        let client: Client<_, hyper::Body> = Client::builder().build(Connector::default());
+        match client.request(req).await {
+            Ok(response) => {
+                if response.status() != StatusCode::ACCEPTED {
+                    let body_bytes = response.into_body().collect().await?.to_bytes();
+                    let response_body = String::from_utf8(body_bytes.to_vec()).unwrap_or_default();
+                    anyhow::bail!("Server did not accept trace stats: {response_body}");
+                }
+                Ok(())
             }
-            Ok(())
+            Err(e) => anyhow::bail!("Failed to send trace stats: {e}"),
         }
-        Err(e) => anyhow::bail!("Failed to send trace stats: {e}"),
     }
 }
 
 #[cfg(test)]
-mod tests {
+#[cfg(feature = "mini_agent")]
+mod mini_agent_tests {
     use crate::stats_utils;
     use datadog_trace_protobuf::pb::{
         ClientGroupedStats, ClientStatsBucket, ClientStatsPayload, Trilean::NotSet,
