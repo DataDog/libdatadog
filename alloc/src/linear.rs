fbdbe3ae2c1c6be83d9c2288e0cd1897db70dfff
Levi Morrison <levi.morrison@datadoghq.com>
2025-03-21 10:03:29 -0600

    chore: update bolero and pin cargo-bundle-licenses (#962)

--- alloc/src/linear.rs
+++ alloc/src/linear.rs
@@ -140,19 +140,19 @@ mod tests {
     use super::*;
     use crate::utils::*;
     use allocator_api2::alloc::Global;
+    use bolero::generator::TypeGenerator;
 
     #[test]
     fn fuzz() {
         // avoid SUMMARY: libFuzzer: out-of-memory
         const MAX_SIZE: usize = 0x10000000;
 
-        use bolero::TypeGenerator;
         let size_hint = 0..=MAX_SIZE;
         let align_bits = 0..=32;
         let size = 0..=MAX_SIZE;
         let idx = 0..=MAX_SIZE;
-        let val = u8::gen();
-        let allocs = Vec::<(usize, u32, usize, u8)>::gen()
+        let val = u8::produce();
+        let allocs = Vec::<(usize, u32, usize, u8)>::produce()
             .with()
             .values((size, align_bits, idx, val));
         bolero::check!()
