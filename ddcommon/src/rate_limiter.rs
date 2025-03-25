0eb4b8911d9efb3a507a080c636d3cc9dd469c72
vianney <vianney.ruhlmann@datadoghq.com>
2025-03-25 18:11:57 +0100

    Fix 32bit in rate-limiter (#964)
    * fix(ddcommon): Support 32bit in rate-limiter
    
    * Update ddcommon/src/rate_limiter.rs
    
    Co-authored-by: Ganesh Jangir <ganesh.jangir@datadoghq.com>
    
    ---------
    
    Co-authored-by: Daniel Schwartz-Narbonne <danielsn@users.noreply.github.com>
    Co-authored-by: Ganesh Jangir <ganesh.jangir@datadoghq.com>

--- ddcommon/src/rate_limiter.rs
+++ ddcommon/src/rate_limiter.rs
@@ -54,7 +54,16 @@ fn now() -> u64 {
             tv_nsec: 0,
         };
         unsafe { libc::clock_gettime(libc::CLOCK_MONOTONIC, &mut ts) };
-        (ts.tv_sec * TIME_PER_SECOND + ts.tv_nsec) as u64
+        // tv_sec is i32 on 32bit architecture
+        // https://sourceware.org/bugzilla/show_bug.cgi?id=16437
+        #[cfg(target_pointer_width = "32")]
+        {
+            (ts.tv_sec as i64 * TIME_PER_SECOND + ts.tv_nsec as i64) as u64
+        }
+        #[cfg(target_pointer_width = "64")]
+        {
+            (ts.tv_sec * TIME_PER_SECOND + ts.tv_nsec) as u64
+        }
     };
     now
 }
