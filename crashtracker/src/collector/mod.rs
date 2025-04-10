0bf8b51b7137b56504c572184ac8ae9bf8c95003
Daniel Schwartz-Narbonne <daniel.schwartznarbonne@datadoghq.com>
2025-04-10 16:02:24 -0400

    [crashtracker] API to enable/disable crash report collection

--- crashtracker/src/collector/mod.rs
+++ crashtracker/src/collector/mod.rs
@@ -18,5 +18,7 @@ pub use additional_tags::{
 };
 pub use api::*;
 pub use counters::{begin_op, end_op, reset_counters, OpTypes};
-pub use crash_handler::{update_config, update_metadata};
+pub use crash_handler::{
+    disable_crashtracker, enable_crashtracker, update_config, update_metadata,
+};
 pub use spans::{clear_spans, clear_traces, insert_span, insert_trace, remove_span, remove_trace};
