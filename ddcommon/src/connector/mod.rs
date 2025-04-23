f1198245a3ca556f558c12d5146d6e8fe64e708c
Aleksandr Pasechnik <aleksandr.pasechnik@datadoghq.com>
2025-04-23 14:58:19 -0400

    chore: are we building the wrong thing?

--- ddcommon/src/connector/mod.rs
+++ ddcommon/src/connector/mod.rs
@@ -113,7 +113,9 @@ mod https {
     // This this actually needs to be done by the user somewhere in their own main.
     // This will only be active on Unix platforms
     #[cfg(all(feature = "fips", not(test)))]
-    fn ensure_crypto_provider_initialized() {}
+    fn ensure_crypto_provider_initialized() {
+        compile_error!("is this what we are trying to compile in coverage?");
+    }
 
     #[cfg(feature = "use_webpki_roots")]
     pub(super) fn build_https_connector_with_webpki_roots() -> anyhow::Result<
