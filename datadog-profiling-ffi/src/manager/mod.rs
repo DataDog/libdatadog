e2d7c7cdfa9db281fed67212201437bae29eddcf
Daniel Schwartz-Narbonne <daniel.schwartznarbonne@datadoghq.com>
2025-05-30 17:04:42 -0400

    more callbacks

--- datadog-profiling-ffi/src/manager/mod.rs
+++ datadog-profiling-ffi/src/manager/mod.rs
@@ -17,6 +17,30 @@ use anyhow::Context;
 use crossbeam_channel::{select, tick, Receiver, Sender};
 use datadog_profiling::{api, internal};
 
+#[repr(C)]
+pub struct ManagedSampleCallbacks {
+    // Static is probably the wrong type here, but worry about that later.
+    converter: extern "C" fn(*mut c_void) -> Sample<'static>,
+    // Resets the sample for reuse.
+    reset: extern "C" fn(*mut c_void),
+    // Called when a sample is dropped (not recycled)
+    drop: extern "C" fn(*mut c_void),
+}
+
+impl ManagedSampleCallbacks {
+    pub fn new(
+        converter: extern "C" fn(*mut c_void) -> Sample<'static>,
+        reset: extern "C" fn(*mut c_void),
+        drop: extern "C" fn(*mut c_void),
+    ) -> Self {
+        Self {
+            converter,
+            reset,
+            drop,
+        }
+    }
+}
+
 pub struct ProfilerManager {
     samples_receiver: Receiver<*mut c_void>,
     samples_sender: Sender<*mut c_void>,
@@ -26,10 +50,8 @@ pub struct ProfilerManager {
     upload_ticker: Receiver<Instant>,
     profile: internal::Profile,
     cpu_sampler_callback: extern "C" fn(*mut internal::Profile),
-    // Static is probably the wrong type here, but worry about that later.
-    sample_converter: extern "C" fn(*mut c_void) -> Sample<'static>,
-    // True if the sample should be reusused after reset, false otherwise.
-    reset_callback: extern "C" fn(*mut c_void) -> bool,
+    upload_callback: extern "C" fn(*mut internal::Profile),
+    sample_callbacks: ManagedSampleCallbacks,
 }
 
 impl ProfilerManager {
@@ -37,8 +59,10 @@ impl ProfilerManager {
         sample_types: &[api::ValueType],
         period: Option<api::Period>,
         cpu_sampler_callback: extern "C" fn(*mut internal::Profile),
+        upload_callback: extern "C" fn(*mut internal::Profile),
         sample_converter: extern "C" fn(*mut c_void) -> Sample<'static>,
-        reset_callback: extern "C" fn(*mut c_void) -> bool,
+        reset_callback: extern "C" fn(*mut c_void),
+        drop_callback: extern "C" fn(*mut c_void),
     ) -> Self {
         let (samples_sender, samples_receiver) = crossbeam_channel::bounded(10);
         let (recycled_samples_sender, recycled_samples_receiver) = crossbeam_channel::bounded(10);
@@ -54,8 +78,8 @@ impl ProfilerManager {
             recycled_samples_sender,
             profile,
             cpu_sampler_callback,
-            sample_converter,
-            reset_callback,
+            upload_callback,
+            sample_callbacks: ManagedSampleCallbacks::new(sample_converter, reset_callback, drop_callback),
         }
     }
 
@@ -66,21 +90,22 @@ impl ProfilerManager {
             select! {
                 recv(self.samples_receiver) -> raw_sample => {
                     let data = raw_sample?;
-                    let sample = (self.sample_converter)(data);
+                    let sample = (self.sample_callbacks.converter)(data);
                     self.profile.add_sample(sample.try_into()?, None)?;
-                    if (self.reset_callback)(data) {
-                        let _ = self.recycled_samples_sender.send(data);
+                    (self.sample_callbacks.reset)(data);
+                    if self.recycled_samples_sender.send(data).is_err() {
+                        (self.sample_callbacks.drop)(data);
                     }
                 },
                 recv(self.cpu_ticker) -> msg => {
                     (self.cpu_sampler_callback)(&mut self.profile);
                 },
                 recv(self.upload_ticker) -> msg => {
-                    let old_profile = self.profile.reset_and_return_previous()?;
+                    let mut old_profile = self.profile.reset_and_return_previous()?;
+                    let upload_callback = self.upload_callback;
                     std::thread::spawn(move || {
-                        if let Ok(encoded) = old_profile.serialize_into_compressed_pprof(None, None) {
-                            println!("Successfully serialized profile");
-                        }
+                        (upload_callback)(&mut old_profile);
+                        // TODO: make sure we cleanup the profile.
                     });
                 },
                 recv(done) -> msg => return Ok(()),
@@ -123,16 +148,25 @@ pub unsafe extern "C" fn ddog_prof_Profile_add_internal(
 mod tests {
     use super::*;
 
-    extern "C" fn test_cpu_sampler_callback(_: *mut datadog_profiling::internal::Profile) {}
+    extern "C" fn test_cpu_sampler_callback(_: *mut datadog_profiling::internal::Profile) {
+        println!("cpu sampler callback");
+    }
+    extern "C" fn test_upload_callback(_: *mut datadog_profiling::internal::Profile) {
+        println!("upload callback");
+    }
     extern "C" fn test_sample_converter(_: *mut c_void) -> Sample<'static> {
+        println!("sample converter");
         Sample {
             locations: ddcommon_ffi::Slice::empty(),
             values: ddcommon_ffi::Slice::empty(),
             labels: ddcommon_ffi::Slice::empty(),
         }
     }
-    extern "C" fn test_reset_callback(_: *mut c_void) -> bool {
-        false
+    extern "C" fn test_reset_callback(_: *mut c_void) {
+        println!("reset callback");
+    }
+    extern "C" fn test_drop_callback(_: *mut c_void) {
+        println!("drop callback");
     }
 
     #[test]
@@ -143,8 +177,10 @@ mod tests {
             &sample_types,
             period,
             test_cpu_sampler_callback,
+            test_upload_callback,
             test_sample_converter,
             test_reset_callback,
+            test_drop_callback,
         );
         println!("start");
         profile_manager.main().unwrap();
