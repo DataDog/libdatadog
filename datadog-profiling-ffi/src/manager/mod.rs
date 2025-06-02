a0e8f25a59ee9758499d35ffca018e6f1dd67d4c
Daniel Schwartz-Narbonne <daniel.schwartznarbonne@datadoghq.com>
2025-06-02 15:56:31 -0400

    send_sample api

--- datadog-profiling-ffi/src/manager/mod.rs
+++ datadog-profiling-ffi/src/manager/mod.rs
@@ -39,11 +39,67 @@ impl ManagedSampleCallbacks {
     }
 }
 
+#[repr(transparent)]
+pub struct SendSample(*mut c_void);
+
+// SAFETY: This type is used to transfer ownership of a sample between threads via channels.
+// The sample is only accessed by one thread at a time, and ownership is transferred along
+// with the SendSample wrapper. The sample is either processed by the manager thread or
+// recycled back to the original thread.
+unsafe impl Send for SendSample {}
+
+impl SendSample {
+    pub fn new(ptr: *mut c_void) -> Self {
+        Self(ptr)
+    }
+
+    pub fn as_ptr(&self) -> *mut c_void {
+        self.0
+    }
+}
+
+pub struct SampleChannels {
+    samples_sender: Sender<SendSample>,
+    recycled_samples_receiver: Receiver<SendSample>,
+}
+
+impl SampleChannels {
+    pub fn new() -> (Self, Receiver<SendSample>, Sender<SendSample>) {
+        let (samples_sender, samples_receiver) = crossbeam_channel::bounded(10);
+        let (recycled_samples_sender, recycled_samples_receiver) = crossbeam_channel::bounded(10);
+        (
+            Self {
+                samples_sender,
+                recycled_samples_receiver,
+            },
+            samples_receiver,
+            recycled_samples_sender,
+        )
+    }
+
+    /// # Safety
+    /// The caller must ensure that:
+    /// 1. The sample pointer is valid and points to a properly initialized sample
+    /// 2. The caller transfers ownership of the sample to this function
+    ///    - The sample is not being used by any other thread
+    ///    - The sample must not be accessed by the caller after this call
+    ///    - The manager will either free the sample or recycle it back
+    /// 3. The sample will be properly cleaned up if it cannot be sent
+    pub unsafe fn send_sample(
+        &self,
+        sample: *mut c_void,
+    ) -> Result<(), crossbeam_channel::SendError<SendSample>> {
+        self.samples_sender.send(SendSample::new(sample))
+    }
+
+    pub fn try_recv_recycled(&self) -> Result<*mut c_void, crossbeam_channel::TryRecvError> {
+        self.recycled_samples_receiver.try_recv().map(|s| s.as_ptr())
+    }
+}
+
 pub struct ProfilerManager {
-    samples_receiver: Receiver<*mut c_void>,
-    samples_sender: Sender<*mut c_void>,
-    recycled_samples_receiver: Receiver<*mut c_void>,
-    recycled_samples_sender: Sender<*mut c_void>,
+    samples_receiver: Receiver<SendSample>,
+    recycled_samples_sender: Sender<SendSample>,
     cpu_ticker: Receiver<Instant>,
     upload_ticker: Receiver<Instant>,
     profile: internal::Profile,
@@ -53,49 +109,48 @@ pub struct ProfilerManager {
 }
 
 impl ProfilerManager {
-    pub fn new(
+    pub fn start(
         sample_types: &[api::ValueType],
         period: Option<api::Period>,
         cpu_sampler_callback: extern "C" fn(*mut internal::Profile),
         upload_callback: extern "C" fn(*mut internal::Profile),
-        sample_converter: extern "C" fn(*mut c_void) -> Sample<'static>,
-        reset_callback: extern "C" fn(*mut c_void),
-        drop_callback: extern "C" fn(*mut c_void),
-    ) -> Self {
-        let (samples_sender, samples_receiver) = crossbeam_channel::bounded(10);
-        let (recycled_samples_sender, recycled_samples_receiver) = crossbeam_channel::bounded(10);
+        sample_callbacks: ManagedSampleCallbacks,
+    ) -> SampleChannels {
+        let (channels, samples_receiver, recycled_samples_sender) = SampleChannels::new();
         let profile = internal::Profile::new(sample_types, period);
         let cpu_ticker = tick(Duration::from_millis(100));
         let upload_ticker = tick(Duration::from_secs(1));
-        Self {
+        let mut manager = Self {
             cpu_ticker,
             upload_ticker,
             samples_receiver,
-            samples_sender,
-            recycled_samples_receiver,
             recycled_samples_sender,
             profile,
             cpu_sampler_callback,
             upload_callback,
-            sample_callbacks: ManagedSampleCallbacks::new(
-                sample_converter,
-                reset_callback,
-                drop_callback,
-            ),
-        }
+            sample_callbacks,
+        };
+
+        std::thread::spawn(move || {
+            if let Err(e) = manager.main() {
+                eprintln!("ProfilerManager error: {}", e);
+            }
+        });
+
+        channels
     }
 
-    pub fn main(&mut self) -> anyhow::Result<()> {
+    fn main(&mut self) -> anyhow::Result<()> {
         // This is just here to allow us to easily bail out.
         let done = tick(Duration::from_secs(5));
         loop {
             select! {
                 recv(self.samples_receiver) -> raw_sample => {
-                    let data = raw_sample?;
+                    let data = raw_sample?.as_ptr();
                     let sample = (self.sample_callbacks.converter)(data);
                     self.profile.add_sample(sample.try_into()?, None)?;
                     (self.sample_callbacks.reset)(data);
-                    if self.recycled_samples_sender.send(data).is_err() {
+                    if self.recycled_samples_sender.send(SendSample::new(data)).is_err() {
                         (self.sample_callbacks.drop)(data);
                     }
                 },
@@ -175,16 +230,19 @@ mod tests {
     fn test_the_thing() {
         let sample_types = [];
         let period = None;
-        let mut profile_manager = ProfilerManager::new(
-            &sample_types,
-            period,
-            test_cpu_sampler_callback,
-            test_upload_callback,
+        let sample_callbacks = ManagedSampleCallbacks::new(
             test_sample_converter,
             test_reset_callback,
             test_drop_callback,
         );
+        let _channels = ProfilerManager::start(
+            &sample_types,
+            period,
+            test_cpu_sampler_callback,
+            test_upload_callback,
+            sample_callbacks,
+        );
         println!("start");
-        profile_manager.main().unwrap();
+        std::thread::sleep(Duration::from_secs(5));
     }
 }
