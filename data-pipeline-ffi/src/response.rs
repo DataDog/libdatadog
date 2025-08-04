b9dcba780ed1451841c5108c665dbb1b280745bf
Julio Gonzalez <julio.gonzalez@datadoghq.com>
2025-08-04 10:53:48 +0200

    Return agent response length.

--- data-pipeline-ffi/src/response.rs
+++ data-pipeline-ffi/src/response.rs
@@ -19,16 +19,19 @@ use std::{
 pub struct ExporterResponse {
     /// The body of the response, which is a string containing the response from the agent.
     pub body: CString,
+    pub body_len: usize,
 }
 
 impl From<AgentResponse> for ExporterResponse {
     fn from(value: AgentResponse) -> Self {
         match value {
             AgentResponse::Changed { body } => ExporterResponse {
+                body_len: body.len(),
                 body: CString::new(body).unwrap_or_default(),
             },
             AgentResponse::Unchanged => ExporterResponse {
                 body: CString::new("").unwrap_or_default(),
+                body_len: 0,
             },
         }
     }
@@ -39,10 +42,14 @@ impl From<AgentResponse> for ExporterResponse {
 #[no_mangle]
 pub unsafe extern "C" fn ddog_trace_exporter_response_get_body(
     response: *const ExporterResponse,
+    out_len: Option<&mut usize>,
 ) -> *const c_char {
     if response.is_null() {
         null()
     } else {
+        if let Some(len) = out_len {
+            *len = (*response).body_len;
+        }
         (*response).body.as_ptr()
     }
 }
@@ -67,20 +74,33 @@ mod tests {
             body: "res".to_string(),
         };
         let response = &ExporterResponse::from(agent_response) as *const ExporterResponse;
+        let mut len = 0;
         let body = unsafe {
-            CStr::from_ptr(ddog_trace_exporter_response_get_body(response)).to_string_lossy()
+            CStr::from_ptr(ddog_trace_exporter_response_get_body(
+                response,
+                Some(&mut len),
+            ))
+            .to_string_lossy()
         };
         assert_eq!(body, "res".to_string());
+        assert_eq!(len, 3);
     }
 
     #[test]
     fn constructor_test_unchanged() {
         let agent_response = AgentResponse::Unchanged;
         let response = Box::into_raw(Box::new(ExporterResponse::from(agent_response)));
+        let mut len = usize::MAX;
         let body = unsafe {
-            CStr::from_ptr(ddog_trace_exporter_response_get_body(response)).to_string_lossy()
+            CStr::from_ptr(ddog_trace_exporter_response_get_body(
+                response,
+                Some(&mut len),
+            ))
+            .to_string_lossy()
         };
         assert_eq!(body, "".to_string());
+        assert_eq!(len, 0);
+
         unsafe {
             ddog_trace_exporter_response_free(response);
         }
@@ -89,7 +109,7 @@ mod tests {
     #[test]
     fn handle_null_test() {
         unsafe {
-            let body = ddog_trace_exporter_response_get_body(null());
+            let body = ddog_trace_exporter_response_get_body(null(), None);
             assert!(body.is_null());
 
             ddog_trace_exporter_response_free(null::<ExporterResponse>() as *mut ExporterResponse);
