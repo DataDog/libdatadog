259212e9a274d03c4aef31692b732a11ab5cae55
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2025-08-13 17:59:33 +0200

    Fix errors

--- datadog-sidecar/src/service/mod.rs
+++ datadog-sidecar/src/service/mod.rs
@@ -72,5 +72,5 @@ pub enum SidecarAction {
     AddTelemetryMetricPoint((String, f64, Vec<Tag>)),
     PhpComposerTelemetryFile(PathBuf),
     ClearQueueId,
-    AddRoute(Route),
+    AddEndpoint(ddtelemetry::data::Endpoint),
 }
