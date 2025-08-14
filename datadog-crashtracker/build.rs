c50fb040f3117bb65254bd0725d448112a9a1ee0
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2025-08-14 17:05:02 +0200

    Add building of test library

--- datadog-crashtracker/build.rs
+++ datadog-crashtracker/build.rs
@@ -1,11 +1,37 @@
 // Copyright 2025-Present Datadog, Inc. https://www.datadoghq.com/
 // SPDX-License-Identifier: Apache-2.0
+use std::process::Command;
+use std::path::Path;
+
+#[cfg(unix)]
+fn build_libtest_so() {
+    let base_path = Path::new("data");
+    let src = base_path.join("libtest.c");
+    let dst = base_path.join("libtest.so");
+    let mut cc_build = Command::new("clang")
+        .arg(src)
+        .arg("-shared")
+        .arg("-fPIC")
+        .arg("-Wl,--version-script,data/libtest.map")
+        .arg("-O0")
+        .arg("-gdwarf-4")
+        // Fix build id to ease in testing.
+        .arg("-Wl,--build-id=0xac33885879e4d40850d3d0fd68a1ac8e0d799dee")
+        .arg("-o")
+        .arg(dst)
+        .spawn()
+        .expect("failed to spawn clang command");
+
+    cc_build.wait().expect("failed to build libtest.so");
+}
 
 #[cfg(unix)]
 fn main() {
     cc::Build::new()
         .file("src/crash_info/emit_sicodes.c")
         .compile("emit_sicodes");
+    // should be put under a feature when testing
+    build_libtest_so();
 }
 
 #[cfg(not(unix))]
