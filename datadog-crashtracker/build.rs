72af2cd2a231765de1f2d69c02aa61e30c93c1fb
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2025-08-15 10:38:50 +0200

    Use cc_utils to make it work on macos

--- datadog-crashtracker/build.rs
+++ datadog-crashtracker/build.rs
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 #[cfg(unix)]
 use std::path::Path;
-#[cfg(unix)]
-use std::process::Command;
+
+pub use cc_utils::cc;
 
 #[cfg(unix)]
 fn build_libtest_so() {
@@ -14,23 +14,22 @@ fn build_libtest_so() {
 
     let src = base_path.join("libtest.c");
     let dst = base_path.join("libtest.so");
-    let mut cc_build = Command::new("cc")
-        .arg(src)
-        .arg("-shared")
-        .arg("-fPIC")
-        // this is needed for the cross compile (cargo cross)
-        .arg("-std=c99")
-        .arg("-Wl,--version-script,data/libtest.map")
-        .arg("-O0")
-        .arg("-gdwarf-4")
-        // Fix build id to ease in testing.
-        .arg("-Wl,--build-id=0xac33885879e4d40850d3d0fd68a1ac8e0d799dee")
-        .arg("-o")
-        .arg(dst)
-        .spawn()
-        .expect("failed to spawn cc command");
 
-    cc_build.wait().expect("failed to build libtest.so");
+    cc_utils::ImprovedBuild::new()
+        .file(src)
+        .link_dynamically("dl")
+        // this is needed for the cross compile (cargo cross)
+        .flag("-std=c99")
+        .flag("-Wl,--version-script,data/libtest.map")
+        // Fix build id to ease in testing.
+        .flag("-Wl,--build-id=0xac33885879e4d40850d3d0fd68a1ac8e0d799dee")
+        .flag("-O0")
+        .flag("-gdwarf-4")
+        .warnings(true)
+        .warnings_into_errors(true)
+        .emit_rerun_if_env_changed(true)
+        .try_compile_shared_lib(dst.to_str().expect("Failed to convert path to str"))
+        .unwrap();
 }
 
 #[cfg(unix)]
