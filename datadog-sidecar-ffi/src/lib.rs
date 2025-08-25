03595cf23f97131c1cf12b6b5b728d0ad6411ae2
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2025-08-25 17:17:33 +0200

    Fix return

--- datadog-sidecar-ffi/src/lib.rs
+++ datadog-sidecar-ffi/src/lib.rs
@@ -411,7 +411,7 @@ pub unsafe extern "C" fn ddog_sidecar_telemetry_addEndpoint(
     path: CharSlice,
     operation_name: CharSlice,
     resource_name: CharSlice,
-) {
+) -> MaybeError {
     let endpoint = TelemetryActions::AddEndpoint(ddtelemetry::data::Endpoint {
         r#type: Some(r#type.to_utf8_lossy().into_owned()),
         method: Some(method),
@@ -426,6 +426,7 @@ pub unsafe extern "C" fn ddog_sidecar_telemetry_addEndpoint(
         queue_id,
         vec![SidecarAction::Telemetry(endpoint)],
     ));
+    MaybeError::None
 }
 
 /// Reports a dependency to the telemetry.
