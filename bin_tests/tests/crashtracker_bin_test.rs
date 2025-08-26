4e6b3f0b6f982dda96a6e52b57024788132d285c
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2025-08-26 16:39:53 +0200

    Check callstack

--- bin_tests/tests/crashtracker_bin_test.rs
+++ bin_tests/tests/crashtracker_bin_test.rs
@@ -129,6 +129,94 @@ fn test_crash_tracking_bin_prechain_sigabrt() {
     test_crash_tracking_bin(BuildProfile::Release, "prechain_abort", "null_deref");
 }
 
+#[test]
+#[cfg_attr(miri, ignore)]
+fn test_crasht_tracking_validate_callstack() {
+    test_crash_tracking_callstack()
+}
+
+fn test_crash_tracking_callstack() {
+    let (_, crashtracker_receiver) = setup_crashtracking_crates(BuildProfile::Release);
+
+    let crashing_app = ArtifactsBuild {
+        name: "crashing_test_app".to_owned(),
+        // compile in debug so we avoid inlining
+        // and can check the callchain
+        build_profile: BuildProfile::Debug,
+        artifact_type: ArtifactType::Bin,
+        triple_target: None,
+    };
+
+    let fixtures = setup_test_fixtures(&[&crashtracker_receiver, &crashing_app]);
+
+    let mut p = process::Command::new(&fixtures.artifacts[&crashing_app])
+        .arg(format!("file://{}", fixtures.crash_profile_path.display()))
+        .arg(fixtures.artifacts[&crashtracker_receiver].as_os_str())
+        .arg(&fixtures.output_dir)
+        .spawn()
+        .unwrap();
+
+    let exit_status = bin_tests::timeit!("exit after signal", {
+        eprintln!("Waiting for exit");
+        p.wait().unwrap()
+    });
+    assert!(!exit_status.success());
+
+    let stderr_path = format!("{0}/out.stderr", fixtures.output_dir.display());
+    let stderr = fs::read(stderr_path)
+        .context("reading crashtracker stderr")
+        .unwrap();
+    let stdout_path = format!("{0}/out.stdout", fixtures.output_dir.display());
+    let stdout = fs::read(stdout_path)
+        .context("reading crashtracker stdout")
+        .unwrap();
+    let s = String::from_utf8(stderr);
+    assert!(
+        matches!(
+            s.as_deref(),
+            Ok("") | Ok("Failed to fully receive crash.  Exit state was: StackTrace([])\n")
+            | Ok("Failed to fully receive crash.  Exit state was: InternalError(\"{\\\"ip\\\": \\\"\")\n"),
+        ),
+        "got {s:?}"
+    );
+    assert_eq!(Ok(""), String::from_utf8(stdout).as_deref());
+
+    let crash_profile = fs::read(fixtures.crash_profile_path)
+        .context("reading crashtracker profiling payload")
+        .unwrap();
+    let crash_payload = serde_json::from_slice::<serde_json::Value>(&crash_profile)
+        .context("deserializing crashtracker profiling payload to json")
+        .unwrap();
+
+    // Note: in Release, we do not have the crate and module name prepended to the function name
+    // Here we compile the crashing app in Debug.
+    let expected_functions: Vec<&str> = [
+        "crashing_test_app::unix::fn3",
+        "crashing_test_app::unix::fn2",
+        "crashing_test_app::unix::fn1",
+        "crashing_test_app::unix::main",
+        "crashing_test_app::main",
+    ]
+    .to_vec();
+
+    let crashing_callstack = &crash_payload["error"]["stack"]["frames"];
+    assert!(
+        crashing_callstack.as_array().unwrap().len() >= expected_functions.len(),
+        "crashing thread callstacks ddddd",
+    );
+
+    let function_names: Vec<&str> = crashing_callstack
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|f| f["function"].as_str().unwrap_or(""))
+        .collect();
+
+    for (expected, actual) in expected_functions.iter().zip(function_names.iter()) {
+        assert_eq!(expected, actual);
+    }
+}
+
 fn test_crash_tracking_bin(
     crash_tracking_receiver_profile: BuildProfile,
     mode: &str,
