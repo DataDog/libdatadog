ff244b9b07942ffe55df96176d6564e9ebe2da17
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2025-10-01 16:41:37 +0200

    Add bin tests for windows

--- bin_tests/src/bin/crashtracker_receiver.rs
+++ bin_tests/src/bin/crashtracker_receiver.rs
@@ -1,10 +1,6 @@
 // Copyright 2023-Present Datadog, Inc. https://www.datadoghq.com/
 // SPDX-License-Identifier: Apache-2.0
 
-#[cfg(not(unix))]
-fn main() {}
-
-#[cfg(unix)]
 fn main() -> anyhow::Result<()> {
     datadog_crashtracker::receiver_entry_point_stdin()
 }
