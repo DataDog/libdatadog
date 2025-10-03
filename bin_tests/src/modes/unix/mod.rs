45335d54f9c824f18188ff502a7479cf9121cd3a
Gyuheon Oh <gyuheon.oh@datadoghq.com>
2025-10-03 18:31:48 +0000

    Add bin tests

--- bin_tests/src/modes/unix/mod.rs
+++ bin_tests/src/modes/unix/mod.rs
@@ -10,3 +10,5 @@ pub mod test_006_sigchld_sigstack;
 pub mod test_007_chaining;
 pub mod test_008_fork;
 pub mod test_009_prechain_with_abort;
+pub mod test_010_runtime_callback_frame;
+pub mod test_011_runtime_callback_string;
