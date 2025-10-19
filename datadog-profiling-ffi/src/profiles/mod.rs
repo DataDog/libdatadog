ffe715d51460b6034738584c2258410aa83a74e1
Levi Morrison <levi.morrison@datadoghq.com>
2025-10-19 13:23:38 +0200

    drop old-dated WIP todo

--- datadog-profiling-ffi/src/profiles/mod.rs
+++ datadog-profiling-ffi/src/profiles/mod.rs
@@ -1,11 +1,6 @@
 // Copyright 2021-Present Datadog, Inc. https://www.datadoghq.com/
 // SPDX-License-Identifier: Apache-2.0
 
-// todo: do we need to expose compressor? Does FFI have any desire to expose
-//       a non-compressed serialized profile?
-// mod compressor;
-// mod parallel_set;
-
 mod pprof_builder;
 mod profile;
 mod profiling_dictionary;
