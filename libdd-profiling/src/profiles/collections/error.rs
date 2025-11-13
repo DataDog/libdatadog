3e1bd42f01f04b655e706cd594904c8a07664367
Levi Morrison <levi.morrison@datadoghq.com>
2025-11-13 15:02:01 -0700

    feat(profiling): Set and StringSet (#1337)
    feat(profiling): Set and StringSet
    
    Reduce PR size
    
    docs: clarify safety comment
    
    Merge branch 'main' into levi/collections
    
    build: fix lock and license file after merge
    
    Merge branch 'main' into levi/collections
    
    Merge branch 'main' into levi/collections
    
    Co-authored-by: levi.morrison <levi.morrison@datadoghq.com>

---
+++ libdd-profiling/src/profiles/collections/error.rs
@@ -0,0 +1,29 @@
+// Copyright 2025-Present Datadog, Inc. https://www.datadoghq.com/
+// SPDX-License-Identifier: Apache-2.0
+
+#[repr(C)]
+#[derive(Debug, thiserror::Error)]
+pub enum SetError {
+    #[error("set error: invalid argument")]
+    InvalidArgument,
+    #[error("set error: out of memory")]
+    OutOfMemory,
+}
+
+impl From<libdd_alloc::AllocError> for SetError {
+    fn from(_: libdd_alloc::AllocError) -> Self {
+        SetError::OutOfMemory
+    }
+}
+
+impl From<std::collections::TryReserveError> for SetError {
+    fn from(_: std::collections::TryReserveError) -> Self {
+        SetError::OutOfMemory
+    }
+}
+
+impl From<hashbrown::TryReserveError> for SetError {
+    fn from(_: hashbrown::TryReserveError) -> Self {
+        SetError::OutOfMemory
+    }
+}
