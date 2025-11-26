ff918ab4fa57bb8c5c60c33136cea696c0653790
Edouard Schweisguth <edouard.schweisguth@datadoghq.com>
2025-11-26 18:26:42 +0100

    Fix licence

--- libdd-trace-normalization/fuzz/fuzz_targets/fuzz_normalize_span.rs
+++ libdd-trace-normalization/fuzz/fuzz_targets/fuzz_normalize_span.rs
@@ -1,3 +1,6 @@
+// Copyright 2023-Present Datadog, Inc. https://www.datadoghq.com/
+// SPDX-License-Identifier: Apache-2.0
+
 #![no_main]
 
 use libdd_trace_normalization::fuzz::{fuzz_normalize_span, FuzzSpan};
