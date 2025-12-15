b8a5fab1f6dff2481f492e1e67d271652074b34e
Jordan <jordan.storms@datadoghq.com>
2025-12-15 13:04:48 -0500

    add test for non-azure-apim spans

--- libdd-trace-utils/src/trace_utils.rs
+++ libdd-trace-utils/src/trace_utils.rs
@@ -1254,6 +1254,32 @@ mod tests {
     }
 
     #[test]
+    fn test_enrich_span_with_azure_function_metadata_adds_tags_for_non_apim() {
+        let mut span = create_test_span(1234, 12342, 12341, 1, false);
+        span.name = "azure.function".to_string();
+
+        enrich_span_with_azure_function_metadata(&mut span);
+
+        // If AAS_METADATA_FUNCTION is available, verify aas.* tags were added
+        // If not available (most test environments), this is a no-op
+        // This test primarily ensures the function doesn't skip non-apim spans
+        if azure_app_services::AAS_METADATA_FUNCTION.is_some() {
+            assert!(span.meta.contains_key("aas.resource.id"));
+            assert!(span.meta.contains_key("aas.environment.instance_id"));
+            assert!(span.meta.contains_key("aas.environment.instance_name"));
+            assert!(span.meta.contains_key("aas.subscription.id"));
+            assert!(span.meta.contains_key("aas.environment.os"));
+            assert!(span.meta.contains_key("aas.environment.runtime"));
+            assert!(span.meta.contains_key("aas.environment.runtime_version"));
+            assert!(span.meta.contains_key("aas.environment.function_runtime"));
+            assert!(span.meta.contains_key("aas.resource.group"));
+            assert!(span.meta.contains_key("aas.site.name"));
+            assert!(span.meta.contains_key("aas.site.kind"));
+            assert!(span.meta.contains_key("aas.site.type"));
+        }
+    }
+
+    #[test]
     fn test_enrich_span_with_azure_function_metadata_skips_azure_apim() {
         let mut span = create_test_span(1234, 12342, 12341, 1, false);
         span.name = "azure.apim".to_string();
