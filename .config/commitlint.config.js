017a7e473c95e5733dff35d39d67a928c9f35343
Igor Unanua <igor.unanua@datadoghq.com>
2026-01-16 09:09:07 +0100

    Check that JIRA ticket is at the end

--- .config/commitlint.config.js
+++ .config/commitlint.config.js
@@ -1,1 +1,25 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+const localPlugin = {
+  rules: {
+    'ticket-at-the-end': (parsed, when = 'always') => {
+      const { subject } = parsed;
+
+      const regex = /\[[A-Z]+-[0-9]+\](?!$)/;
+
+      const isValid = !regex.test(subject.trim());
+      const negated = when === 'never';
+
+      return [
+        negated ? !isValid : isValid,
+        `A ticket (e.g. [ABC-123]), if included, must ${negated ? 'not ' : ''}be at the end of the subject`,
+      ];
+    },
+  }
+}
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'ticket-at-the-end': [2, 'always'],
+  },
+  plugins: [localPlugin]
+}
