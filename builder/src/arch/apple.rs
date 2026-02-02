5e0cd62f6905a31cc02ce9db35245a596dccbeb0
Ivo Anjo <ivo.anjo@datadoghq.com>
2026-02-02 15:14:54 +0000

    fix: Make sure build fails if command returns non-zero code
    **What does this PR do?**
    
    This PR introduces a `wait_for_success` helper and uses it throughout
    the builder crate to ensure that the status code for a command is
    checked, and the build fails if it is non-zero.
    
    **Motivation:**
    
    I ran into this very sharp edge as I was experimenting with some
    build changes in libdatadog: whenever an external command exits
    with a non-zero status code, the build would continue, rather
    than fail immediately.
    
    This happens because we were not previously checking for the
    command status code. This change fixes this.
    
    **Additional Notes:**
    
    N/A
    
    **How to test the change?**
    
    Since the error code only gets triggered when something goes wrong,
    you won't see it by default. To test this, I replaced command
    invocations (to e.g. `cargo` in `profiling.rs`) with a call to `false`:
    this is an actual command that runs successfully, but then returns
    a non-zero code and thus can be used to see this change in action.

--- builder/src/arch/apple.rs
+++ builder/src/arch/apple.rs
@@ -1,10 +1,11 @@
 // Copyright 2024-Present Datadog, Inc. https://www.datadoghq.com/
 // SPDX-License-Identifier: Apache-2.0
 
+use std::ffi::OsStr;
 use std::os::unix::process::ExitStatusExt;
 use std::process::Command;
 
-use std::ffi::OsStr;
+use crate::utils::wait_for_success;
 
 pub const NATIVE_LIBS: &str =
     " -framework Security -framework CoreFoundation -liconv -lSystem -lresolv -lc -lm -liconv";
@@ -43,13 +44,13 @@ pub fn fix_rpath(lib_path: &str) {
 
 pub fn strip_libraries(lib_path: &str) {
     // objcopy is not available in macos image. Investigate llvm-objcopy
-    let mut strip = Command::new("strip")
+    let strip = Command::new("strip")
         .arg("-S")
         .arg(lib_path.to_owned() + "/libdatadog_profiling.dylib")
         .spawn()
         .expect("Failed to spawn strip");
 
-    strip.wait().expect("Failed to strip library");
+    wait_for_success(strip, "strip");
 }
 
 pub fn add_additional_files(_lib_path: &str, _target_path: &OsStr) {}
--- builder/src/utils.rs
+++ builder/src/utils.rs
@@ -5,6 +5,7 @@ use anyhow::{anyhow, Result};
 use std::fs::{self, OpenOptions};
 use std::io::Write;
 use std::path::{Path, PathBuf};
+use std::process::Child;
 
 pub fn file_replace(file_in: &str, file_out: &str, target: &str, replace: &str) -> Result<()> {
     let content = fs::read_to_string(file_in)?;
@@ -25,4 +26,14 @@ pub fn project_root() -> PathBuf {
         .nth(1)
         .unwrap()
         .to_path_buf()
+}
+
+/// Waits for a child process to complete and panics if it fails.
+pub fn wait_for_success(mut child: Child, name: &str) {
+    let status = child.wait().expect(&format!("{name} failed to wait"));
+    assert!(
+        status.success(),
+        "{name} failed with exit code: {:?}",
+        status.code()
+    );
 }
