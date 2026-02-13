f17efaae2ed5f1d7f2e5c070178c33444ab15723
Jules Wiriath <jules.wiriath@datadoghq.com>
2026-02-13 12:04:12 +0100

    feat: allow cross boundary default client

---
+++ libdd-provider/src/lib.rs
@@ -0,0 +1,16 @@
+// Copyright 2026-Present Datadog, Inc. https://www.datadoghq.com/
+// SPDX-License-Identifier: Apache-2.0
+
+//! Platform capability provider.
+//!
+//! Re-exports the correct implementation of each capability trait based on
+//! compile target. Downstream code uses these types directly, avoiding
+//! generic contagion.
+
+pub use libdd_capabilities::HttpClientTrait;
+
+#[cfg(not(target_arch = "wasm32"))]
+pub use libdd_common::capabilities::HyperHttpClient as DefaultHttpClient;
+
+#[cfg(target_arch = "wasm32")]
+pub use capabilities::WasmHttpClient as DefaultHttpClient;
