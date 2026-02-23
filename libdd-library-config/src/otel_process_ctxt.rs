e4a7e869b1104b32e8774dc51a3e1fabf1e193df
Yann Hamdaoui <yann.hamdaoui@datadoghq.com>
2026-02-23 16:12:01 +0100

    test: free mapping in otel process ctxt test

--- libdd-library-config/src/otel_process_ctxt.rs
+++ libdd-library-config/src/otel_process_ctxt.rs
@@ -452,6 +452,8 @@ pub mod linux {
             );
             assert!(header.published_at_ns > 0, "published_at_ns is zero");
             assert!(read_payload == payload.as_bytes(), "payload mismatch");
+
+            super::unpublish().expect("couldn't unpublish the context");
         }
     }
 }
