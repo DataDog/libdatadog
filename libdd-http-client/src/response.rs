5d99312ef4897b4e9523e54a9fdbccdcc27d67e0
Edmund Kump <edmund.kump@datadoghq.com>
2026-02-26 14:13:25 +0100

    add response and fips tests

--- libdd-http-client/src/response.rs
+++ libdd-http-client/src/response.rs
@@ -15,3 +15,44 @@ pub struct HttpResponse {
     /// Response body bytes.
     pub body: bytes::Bytes,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn fields_accessible() {
+        let resp = HttpResponse {
+            status_code: 200,
+            headers: vec![("content-type".to_owned(), "application/json".to_owned())],
+            body: bytes::Bytes::from_static(b"{\"ok\":true}"),
+        };
+        assert_eq!(resp.status_code, 200);
+        assert_eq!(resp.headers.len(), 1);
+        assert_eq!(resp.headers[0].0, "content-type");
+        assert_eq!(resp.headers[0].1, "application/json");
+        assert_eq!(resp.body.as_ref(), b"{\"ok\":true}");
+    }
+
+    #[test]
+    fn empty_response() {
+        let resp = HttpResponse {
+            status_code: 204,
+            headers: vec![],
+            body: bytes::Bytes::new(),
+        };
+        assert_eq!(resp.status_code, 204);
+        assert!(resp.headers.is_empty());
+        assert!(resp.body.is_empty());
+    }
+
+    #[test]
+    fn debug_includes_status() {
+        let resp = HttpResponse {
+            status_code: 404,
+            headers: vec![],
+            body: bytes::Bytes::new(),
+        };
+        assert!(format!("{resp:?}").contains("404"));
+    }
+}
