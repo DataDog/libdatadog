2afbf2118494ccb4766e6b68f9312714e937d655
Loic Nageleisen <loic.nageleisen@gmail.com>
2026-03-13 23:36:21 +0100

    chore: Remove Ruby bits (#1725)
    # What does this PR do?
    
    Remove Ruby bits
    
    # Motivation
    
    These have been migrated to: https://github.com/DataDog/libdatadog-rb
    
    # Additional Notes
    
    As an alternative it was suggested to leave a README explaining the migration, but I don't think any interested party would be surprised since they are all aware of the change.
    
    # How to test the change?
    
    Non-ruby things do not break?
    
    Co-authored-by: edmund.kump <edmund.kump@datadoghq.com>

--- ruby/publish/rakefile.rb
+++
@@ -1,8 +0,0 @@
-# This file is used so that the Ruby `rake` command can be used from the root of the repository.
-# This is needed for the publish-ruby.yml CI workflow to work.
-
-require "rake"
-
-Dir.chdir("ruby")
-Rake.application.add_import("Rakefile")
-Rake.application.load_imports
