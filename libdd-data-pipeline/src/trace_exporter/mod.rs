583679e93aff011c7e9b5be1f157e134e5861982
Bob Weinand <bob.weinand@datadoghq.com>
2026-03-30 17:20:38 +0200

    clippy
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- libdd-data-pipeline/src/trace_exporter/mod.rs
+++ libdd-data-pipeline/src/trace_exporter/mod.rs
@@ -36,7 +36,7 @@ use libdd_common::{http_common, Endpoint};
 use libdd_common::{HttpClient, MutexExt};
 use libdd_dogstatsd_client::Client;
 use libdd_telemetry::worker::TelemetryWorker;
-use libdd_trace_stats::stats_exporter::{StatsExporter, StatsMetadata};
+use libdd_trace_stats::stats_exporter::StatsExporter;
 use libdd_trace_utils::msgpack_decoder;
 use libdd_trace_utils::send_with_retry::{
     send_with_retry, RetryStrategy, SendWithRetryError, SendWithRetryResult,
