936e4413dcc7aeea8a30738841b76ddca20828ad
Yann Hamdaoui <yann.hamdaoui@datadoghq.com>
2026-04-07 11:27:29 +0200

    perf: test with inline annots (and more idiomatic iters)

--- libdd-trace-utils/src/change_buffer/mod.rs
+++ libdd-trace-utils/src/change_buffer/mod.rs
@@ -156,16 +156,19 @@ where
     }
 
     /// Diagnostic: number of spans currently in the HashMap.
+    #[inline]
     pub fn spans_count(&self) -> usize {
         self.spans.len()
     }
 
     /// Diagnostic: string table length.
+    #[inline]
     pub fn string_table_len(&self) -> usize {
         self.string_table.len()
     }
 
     /// Diagnostic: span pool size.
+    #[inline]
     pub fn span_pool_len(&self) -> usize {
         self.span_pool.len()
     }
@@ -497,12 +500,14 @@ where
             .ok_or(ChangeBufferError::SpanNotFound(*id))
     }
 
+    #[inline]
     pub fn get_span(&self, id: &u64) -> Result<&Span<T>> {
         self.spans
             .get(id)
             .ok_or(ChangeBufferError::SpanNotFound(*id))
     }
 
+    #[inline]
     pub fn get_trace(&self, id: &u128) -> Option<&Trace<T::Text>> {
         self.traces.get(id)
     }
@@ -573,9 +578,7 @@ where
             existing.trace_id = span.trace_id;
             existing.parent_id = span.parent_id;
             // meta/metrics already populated by change buffer ops
-            for (k, v) in &self.default_meta {
-                existing.meta.insert(k.clone(), v.clone());
-            }
+            existing.meta.extend(self.default_meta.iter().cloned());
         } else {
             self.spans.insert(span_id, span);
         }
@@ -590,6 +593,7 @@ where
     }
 
     /// Get a mutable reference to a span.
+    #[inline]    
     pub fn span_mut(&mut self, id: &u64) -> Result<&mut Span<T>> {
         self.spans
             .get_mut(id)
@@ -597,6 +601,7 @@ where
     }
 
     /// Look up a string by ID, returning a clone.
+    #[inline]
     pub fn get_string(&self, id: u32) -> Option<T::Text> {
         self.string_table
             .get(id as usize)
@@ -606,15 +611,14 @@ where
     /// Set default meta tags that are automatically applied to every new span.
     /// Call this once at init time with the config tags (service, version,
     /// runtime-id, etc.).
+    #[inline]
     pub fn set_default_meta(&mut self, tags: Vec<(T::Text, T::Text)>) {
         self.default_meta = tags;
     }
 
     /// Apply default meta tags to a span.
     fn apply_default_meta(&self, span: &mut Span<T>) {
-        for (key, value) in &self.default_meta {
-            span.meta.insert(key.clone(), value.clone());
-        }
+        span.meta.extend(self.default_meta.iter().cloned());
     }
 
     fn interpret_operation(&mut self, index: &mut usize, op: &BufferedOperation) -> Result<()> {
@@ -746,14 +750,16 @@ where
         Ok(())
     }
 
+    #[inline]
     pub fn string_table_insert_one(&mut self, key: u32, val: T::Text) {
         let idx = key as usize;
         if idx >= self.string_table.len() {
-            self.string_table.resize_with(idx + 1, || None);
+            self.string_table.push(None);
         }
         self.string_table[idx] = Some(val);
     }
 
+    #[inline]
     pub fn string_table_evict_one(&mut self, key: u32) {
         let idx = key as usize;
         if idx < self.string_table.len() {
