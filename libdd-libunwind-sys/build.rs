11deb8355da570d5a336ab9bceea421b8f20f28a
Julio <julio.gonzalez@datadoghq.com>
2026-04-09 11:38:37 +0200

    chore: fix compilation for other platforms

--- libdd-libunwind-sys/build.rs
+++ libdd-libunwind-sys/build.rs
@@ -1,7 +1,16 @@
 // Copyright 2025-Present Datadog, Inc. https://www.datadoghq.com/
 // SPDX-License-Identifier: Apache-2.0
 
-#[cfg_attr(target_os = "linux", path = "build/linux.rs")]
+#[cfg(target_os = "linux")]
+#[path = "build/linux.rs"]
+mod build;
+
+#[cfg(target_os = "windows")]
+#[path = "build/windows.rs"]
+mod build;
+
+#[cfg(target_os = "macos")]
+#[path = "build/macos.rs"]
 mod build;
 
 fn main() {
---
+++ libdd-libunwind-sys/build/macos.rs
@@ -0,0 +1,6 @@
+// Copyright 2026-Present Datadog, Inc. https://www.datadoghq.com/
+// SPDX-License-Identifier: Apache-2.0
+
+pub fn main() {
+    eprintln!("Macos compilation not supported");
+}
