50c04691c1a7ff06aa12f3a0379b37ae32e3f574
Oscar Le Dauphin <oscar.ledauphin@datadoghq.com>
2026-04-13 16:07:03 +0200

    fix: last clippy error

--- libdd-data-pipeline/src/trace_exporter/stats.rs
+++ libdd-data-pipeline/src/trace_exporter/stats.rs
@@ -39,6 +39,7 @@ pub(crate) const STATS_ENDPOINT: &str = "/v0.6/stats";
 /// The maximum obfuscation version this tracer supports.
 #[cfg(feature = "stats-obfuscation")]
 pub(crate) const SUPPORTED_OBFUSCATION_VERSION: u32 = 1;
+#[cfg(feature = "stats-obfuscation")]
 pub(crate) const SUPPORTED_OBFUSCATION_VERSION_STR: &str = "1";
 
 #[cfg(not(target_arch = "wasm32"))]
