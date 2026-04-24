2d37b73a472d628fde2dc03b97cc774244b3ff2f
Oscar Le Dauphin <oscar.ledauphin@datadoghq.com>
2026-04-24 14:41:27 +0200

    feat(size-benchmark): simplify to pure function-pointer array approach

--- size-benchmark/src/main.rs
+++ size-benchmark/src/main.rs
@@ -1,19 +1,19 @@
 // Copyright 2025-Present Datadog, Inc. https://www.datadoghq.com/
 // SPDX-License-Identifier: Apache-2.0
 
-// Force-link all FFI crates so their #[no_mangle] symbols are available to the
-// extern "C" stubs in the generated calls.rs.
+// Pull in all FFI crates so their symbols are available to the extern "C" block below.
 extern crate datadog_ffe_ffi;
 extern crate datadog_profiling_ffi;
+extern crate libdd_common_ffi;
 extern crate libdd_crashtracker_ffi;
 extern crate libdd_data_pipeline_ffi;
+extern crate libdd_ddsketch_ffi;
+extern crate libdd_library_config_ffi;
+extern crate libdd_log_ffi;
 extern crate libdd_shared_runtime_ffi;
 extern crate libdd_telemetry_ffi;
 extern crate symbolizer_ffi;
 
-include!(concat!(env!("OUT_DIR"), "/calls.rs"));
+include!(concat!(env!("OUT_DIR"), "/fptrs.rs"));
 
-fn main() {
-    exercise_all();
-    println!("done");
-}
+fn main() {}
