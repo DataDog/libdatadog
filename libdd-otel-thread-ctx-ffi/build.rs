f73161959c9931b53e4c51fc96ae5f09e62187d1
Yann Hamdaoui <yann.hamdaoui@datadoghq.com>
2026-04-24 17:57:53 +0200

    ci: fix build error by relaxing need for system wide lld

--- libdd-otel-thread-ctx-ffi/build.rs
+++ libdd-otel-thread-ctx-ffi/build.rs
@@ -17,11 +17,15 @@ fn main() {
     // Passing our own version script with an explicit `global:` entry for the
     // symbol beats the `local: *` wildcard and prevents that relaxation.
     //
-    // Merging multiple version scripts is not supported by GNU ld, so we also
-    // force lld explicitly.
+    // Merging multiple version scripts is not supported by GNU ld, but lld (or rather rust-lld) is
+    // the default linker picked by Rust since v1.70+. We use to force lld via:
+    // `println!("cargo:rustc-cdylib-link-arg=-fuse-ld=lld")` but this caused extra pain in CI
+    // because it requires a system wide lld install (instead of using rust-lld). Since we have a
+    // test to check that the symbol was properly propagated, we rather let Rust pick lld which is
+    // preferred anyway, and if it's not possible in an exotic build setup, the test should catch
+    // it.
     if target_os == "linux" {
         let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-        println!("cargo:rustc-cdylib-link-arg=-fuse-ld=lld");
         println!(
             "cargo:rustc-cdylib-link-arg=-Wl,--version-script={manifest_dir}/tls-dynamic-list.txt"
         );
