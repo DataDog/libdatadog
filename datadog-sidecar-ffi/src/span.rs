41fc1ee7fe89ba8964328faa394e81ab1c4f3b18
Yann Hamdaoui <yann.hamdaoui@datadoghq.com>
2026-05-21 11:14:21 +0200

    doc: remove TODO after clarification

--- datadog-sidecar-ffi/src/span.rs
+++ datadog-sidecar-ffi/src/span.rs
@@ -141,7 +141,6 @@ pub extern "C" fn ddog_get_traces_size(traces: &TracesBytes) -> usize {
     traces.len()
 }
 
-// TODO: I don't think we need to return a mutable reference here
 #[no_mangle]
 pub extern "C" fn ddog_get_trace(
     traces: &mut TracesBytes,
@@ -162,7 +161,6 @@ pub extern "C" fn ddog_get_trace_size(trace: &TraceBytes) -> usize {
     trace.len()
 }
 
-// TODO:I don't think we need to return a mutable reference here
 #[no_mangle]
 pub extern "C" fn ddog_get_span(trace: &mut TraceBytes, index: usize) -> Option<&mut SpanBytes> {
     trace.get_mut(index)
