1dec1b0eab2e333a6825e8ad8d324b8194fa2fbe
Oscar Le Dauphin <oscar.ledauphin@datadoghq.com>
2026-06-11 18:12:38 +0200

    fix: new copyright dates

--- libdd-data-pipeline/tests/test_trace_filters.rs
+++ libdd-data-pipeline/tests/test_trace_filters.rs
@@ -1,4 +1,4 @@
-// Copyright 2024-Present Datadog, Inc. https://www.datadoghq.com/
+// Copyright 2026-Present Datadog, Inc. https://www.datadoghq.com/
 // SPDX-License-Identifier: Apache-2.0
 
 use std::{
