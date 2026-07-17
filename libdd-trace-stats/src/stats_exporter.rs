8e120341e7e39f898451f2433c9d0ec0f970b8e6
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-07-17 15:52:56 +0000

    fix(trace-stats): disable retries for stats payload

--- libdd-trace-stats/src/stats_exporter.rs
+++ libdd-trace-stats/src/stats_exporter.rs
@@ -17,7 +17,7 @@ use libdd_capabilities::{HttpClientCapability, MaybeSend, SleepCapability};
 use libdd_common::Endpoint;
 use libdd_shared_runtime::Worker;
 use libdd_trace_protobuf::pb;
-use libdd_trace_utils::send_with_retry::{send_with_retry, RetryStrategy};
+use libdd_trace_utils::send_with_retry::{send_with_retry, RetryBackoffType, RetryStrategy};
 use libdd_trace_utils::trace_utils::TracerHeaderTags;
 use libdd_trace_utils::tracer_metadata::TracerMetadata;
 use std::fmt::Debug;
@@ -248,7 +248,7 @@ impl<Cap: HttpClientCapability + SleepCapability, Con: FlushableConcentrator>
             &self.endpoint,
             body,
             &headers,
-            &RetryStrategy::default(),
+            &RetryStrategy::new(0, 0, RetryBackoffType::Constant, None),
         )
         .await;
 
