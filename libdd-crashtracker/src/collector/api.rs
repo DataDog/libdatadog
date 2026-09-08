b2be0c51efe9bba34450299a26f6acb5d1b7d27c
Gyuheon Oh <gyuheon.oh@datadoghq.com>
2026-09-08 14:45:17 +0000

    clean up gating

--- libdd-crashtracker/src/collector/api.rs
+++ libdd-crashtracker/src/collector/api.rs
@@ -87,9 +87,10 @@ pub fn init(
     register_crash_handlers(&config)?;
     register_panic_hook()?;
     #[cfg(all(target_os = "linux", target_pointer_width = "64"))]
-    super::assert_interceptor::install_assert_hook();
-    #[cfg(all(target_os = "linux", target_pointer_width = "64"))]
-    super::sigaction_interceptor::install_sigaction_hook(config.signals());
+    {
+        super::assert_interceptor::install_assert_hook();
+        super::sigaction_interceptor::install_sigaction_hook(config.signals());
+    }
     enable();
     Ok(())
 }
